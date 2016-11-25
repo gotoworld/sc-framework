b7bc0a747a0affe8a8b6d8e6d7c7c2dc3b0bdc90
wu1g119 <aa1230aa>
2016-11-25 16:41:59 +0800

    init

---
+++ wu1g-framework/framework-service/src/main/java/com/wu1g/framework/service/BaseService.java
@@ -0,0 +1,66 @@
+/*
+ * 基础Service类接口
+ *
+ * VERSION  DATE        BY              REASON
+ * -------- ----------- --------------- ------------------------------------------
+ * 1.00     2015.01.13  wuxiaogang      程序・发布
+ * -------- ----------- --------------- ------------------------------------------
+ * Copyright 2015 baseos  System. - All Rights Reserved.
+ *
+ */
+package com.wu1g.framework.service;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.stereotype.Service;
+
+import java.util.Observable;
+
+/**
+ * <p>
+ * service 基类 放置service通用资源或公共方法
+ * </p>
+ * <ol>
+ * [提供机能]
+ * <li>taskExecutor 线程池
+ * <li>jedisHelper 缓存服务
+ * <li>alog 系统_管理员操作日志
+ * <li>Subject 权限验证
+ * </ol>
+ */
+@Service
+public abstract class BaseService extends Observable {
+	/** 线程池 */
+	@Autowired
+	protected TaskExecutor			taskExecutor;
+	/** 用户登录id */
+	protected String				uid;
+	/** 权限验证框架 */
+	private Subject					auth;
+
+	/**
+	 * 用户登录id取得
+	 * 
+	 * @return 用户登录id
+	 */
+	public String getUid() {
+		if (uid == null) {
+			uid = (String) SecurityUtils.getSubject().getPrincipal();
+		}
+		return uid;
+	}
+
+	/**
+	 * 权限验证框架取得
+	 * 
+	 * @return 权限验证框架
+	 */
+	public Subject getAuth() {
+		if (auth == null) {
+			auth = SecurityUtils.getSubject();
+		}
+		return auth;
+	}
+}

915c0f4731242589a7f61476273ea76385ede143
wu1g119 <wu1g@163.com>
2017-03-12 20:29:55 +0800

    x

--- wu1g-servers/wu1g-web-server/src/main/java/com/wu1g/Application.java
+++ wu1g-servers/wu1g-web-server/src/main/java/com/wu1g/Application.java
@@ -1,12 +1,12 @@
 package com.wu1g;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@EnableAutoConfiguration
+@ServletComponentScan
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     @Override

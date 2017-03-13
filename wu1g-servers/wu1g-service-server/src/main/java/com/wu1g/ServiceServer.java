a9c65d1a8936e74a8295c0b0ffc57e79d070dfa4
wu1g119 <aa1230aa>
2017-03-13 10:37:32 +0800

    样式修改

--- wu1g-servers/wu1g-service-server/src/main/java/com/wu1g/ServiceServer.java
+++ wu1g-servers/wu1g-service-server/src/main/java/com/wu1g/ServiceServer.java
@@ -4,8 +4,6 @@ package com.wu1g;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -18,8 +16,8 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableCircuitBreaker
 @EnableAutoConfiguration
 @SpringBootApplication
-public class Application{
+public class ServiceServer {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ServiceServer.class, args);
     }
 }

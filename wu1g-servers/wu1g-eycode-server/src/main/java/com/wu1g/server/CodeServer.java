3b83f7bc20f1e2642ffee496dd1b25e6520ddf9e
wu1g119 <aa1230aa>
2017-03-13 14:59:42 +0800

    x

---
+++ wu1g-servers/wu1g-eycode-server/src/main/java/com/wu1g/server/CodeServer.java
@@ -0,0 +1,11 @@
+package com.wu1g.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeServer {
+    public static void main(String[] args) {
+        SpringApplication.run(CodeServer.class, args);
+    }
+}

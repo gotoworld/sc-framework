b75b0134fba4aabf7832e3ba9c8da105937b1a4f
wu1g119 <aa1230aa>
2017-03-30 18:26:00 +0800

    portal update

---
+++ wu1g-portal/wu1g-portal-web/src/main/java/com/wu1g/web/controller/portal/PortalController.java
@@ -0,0 +1,69 @@
+package com.wu1g.web.controller.portal;
+
+import com.wu1g.api.sys.ISysCategoryService;
+import com.wu1g.api.sys.ISysNewsService;
+import com.wu1g.web.controller.BaseController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * <p>门户首页。
+ */
+@Controller
+@Slf4j
+public class PortalController extends BaseController {
+	@Autowired
+	protected ISysCategoryService sysCategoryService;
+	@Autowired
+	protected ISysNewsService sysNewsService;
+
+	private static final String acPrefix="/";
+	private static final String index = "client/index";
+	private static final String channel = "client/channel";
+	private static final String news = "client/news";
+	private static final String tour = "client/tour";
+	private static final String video = "client/video";
+	/**
+	 * <p> 首页。
+	 */
+	@RequestMapping(method={RequestMethod.GET},value="/")
+	public String index() {
+		log.info("SysNewsController index.........");
+		return index;
+	}
+	/**
+	 * <p> 栏目。
+	 */
+	@RequestMapping(method={RequestMethod.GET},value=acPrefix+"channel")
+	public String channel() {
+		log.info("SysNewsController channel.........");
+		return channel;
+	}
+	/**
+	 * <p> 资讯。
+	 */
+	@RequestMapping(method={RequestMethod.GET},value=acPrefix+"news")
+	public String news() {
+		log.info("SysNewsController news.........");
+		return news;
+	}
+	/**
+	 * <p> 全景图。
+	 */
+	@RequestMapping(method={RequestMethod.GET},value=acPrefix+"tour")
+	public String tour() {
+		log.info("SysNewsController tour.........");
+		return tour;
+	}
+	/**
+	 * <p> 全景视频。
+	 */
+	@RequestMapping(method={RequestMethod.GET},value=acPrefix+"video")
+	public String video() {
+		log.info("SysNewsController video.........");
+		return video;
+	}
+}

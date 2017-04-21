8fc355164d95a3330adaf549dd535609b5e7e907
wu1g119 <aa1230aa>
2017-04-21 18:44:30 +0800

    style update

--- wu1g-servers/wu1g-web-server/src/main/java/com/wu1g/Application.java
+++ wu1g-servers/wu1g-web-server/src/main/java/com/wu1g/Application.java
@@ -1,7 +1,9 @@
 package com.wu1g;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;

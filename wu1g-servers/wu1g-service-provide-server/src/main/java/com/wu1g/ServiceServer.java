0ede486e8b59fc2d7866cbc99700cc98b506152c
wu1g119 <aa1230aa>
2017-04-27 20:36:52 +0800

    结构变更 新增demo

--- wu1g-servers/wu1g-service-provide-server/src/main/java/com/wu1g/ServiceServer.java
+++ wu1g-servers/wu1g-service-provide-server/src/main/java/com/wu1g/ServiceServer.java
@@ -4,14 +4,7 @@ package com.wu1g;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 
 //@EnableHystrix
@@ -21,11 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceServer {
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ServiceServer.class, args);
     }

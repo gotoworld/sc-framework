3bad5de307189afe746d4f617efffc799ea81859
wu1g119 <aa1230aa>
2017-05-03 15:13:30 +0800

    feign provider

---
+++ wu1g-pano/wu1g-pano-services/wu1g-pano-server/src/main/java/com/wu1g/feign/provider/pano/PanoCommentsServiceFeign.java
@@ -0,0 +1,10 @@
+package com.wu1g.feign.provider.pano;
+
+import com.wu1g.service.pano.PanoCommentsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PanoCommentsServiceFeign extends PanoCommentsService {
+}
---
+++ wu1g-pano/wu1g-pano-services/wu1g-pano-server/src/main/java/com/wu1g/feign/provider/pano/PanoSceneServiceFeign.java
@@ -0,0 +1,11 @@
+package com.wu1g.feign.provider.pano;
+
+
+import com.wu1g.service.pano.PanoSceneService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PanoSceneServiceFeign extends PanoSceneService {
+}

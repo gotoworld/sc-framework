cd3283e8d29864eb8c9e8ae747d4baf9b441ccc0
wu1g119 <aa1230aa>
2017-05-03 15:20:13 +0800

    feign provider

--- wu1g-servers/wu1g-service-provide-server/src/main/java/com/wu1g/ServiceServer.java
+++ wu1g-servers/wu1g-service-provide-server/src/main/java/com/wu1g/ServiceServer.java
@@ -4,15 +4,14 @@ package com.wu1g;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 //@EnableHystrix
 //@EnableCircuitBreaker
-
 @EnableAutoConfiguration
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ServiceServer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ServiceServer.class, args);

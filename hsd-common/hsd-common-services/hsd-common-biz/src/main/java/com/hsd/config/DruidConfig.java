e09adad330e36b19f9bfabe1a295cb1588005a76
wuxiaogang <wuxiaogang>
2017-07-12 13:54:52 +0800

    init

---
+++ hsd-common/hsd-common-services/hsd-common-biz/src/main/java/com/hsd/config/DruidConfig.java
@@ -0,0 +1,104 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2016 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.hsd.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
+import com.hsd.framework.config.AppConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+@AutoConfigureAfter(AppConfig.class)
+@Slf4j
+@Order(-1000)
+public class DruidConfig implements EnvironmentAware {
+    private RelaxedPropertyResolver myResolver;
+    @Override
+    public void setEnvironment(Environment env) {
+        this.myResolver = new RelaxedPropertyResolver(env, "hsd.");
+    }
+
+    /**
+     * Druid Filter配置
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter( new WebStatFilter() );
+        filterRegistrationBean.addUrlPatterns( "/*" );
+        filterRegistrationBean.addInitParameter( "exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*" );
+        filterRegistrationBean.addInitParameter( "sessionStatMaxCount", "2000" );
+        filterRegistrationBean.addInitParameter( "sessionStatEnable", "true" );
+        filterRegistrationBean.addInitParameter( "principalSessionName", "session_user_key" );
+        filterRegistrationBean.addInitParameter( "profileEnable", "true" );
+        return filterRegistrationBean;
+    }
+
+    /**
+     * Druid Servlet
+     */
+    @Bean
+    public ServletRegistrationBean dispatcherRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean();
+        // 添加druidServlet
+        registration.setServlet( new StatViewServlet() );
+        registration.addInitParameter( "resetEnable", "true" );
+        registration.addInitParameter( "loginUsername", "druidAdmin" );
+        registration.addInitParameter( "loginPassword", "druidAdmin-123" );
+        registration.addUrlMappings( "/druid/*" );
+        return registration;
+    }
+    @Bean
+    public DruidStatInterceptor druidStatInterceptor() {
+        return new DruidStatInterceptor();
+    }
+
+    /**
+     * 添加Spring监控 监控所有包含Dao和Service的Bean
+     * @return
+     */
+    @Bean
+    public BeanNameAutoProxyCreator beanNameAutoProxyCreator() {
+        BeanNameAutoProxyCreator creator = new BeanNameAutoProxyCreator();
+        creator.setProxyTargetClass( true );
+        creator.setBeanNames( new String[] { "*Dao*", "*Service*" } );
+        creator.setInterceptorNames( "druidStatInterceptor" );
+
+        return creator;
+    }
+}

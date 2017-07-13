f8fe74feac0f57da833c2b890a974ad10d5683ae
uname.chen <uname.chen@gmail.com>
2017-07-13 18:20:11 +0800

    init

--- hsd-servers/hsd-app-starter/hsd-web-portal-server/src/main/java/com/hsd/Application.java
+++ hsd-servers/hsd-app-starter/hsd-web-portal-server/src/main/java/com/hsd/Application.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @ServletComponentScan
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {

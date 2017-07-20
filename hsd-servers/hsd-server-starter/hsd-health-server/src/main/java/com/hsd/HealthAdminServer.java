1331b47f4114bb4438776aeffba397cb3e2276d0
wuxiaogang <wuxiaogang>
2017-07-20 17:00:09 +0800

    init

---
+++ hsd-servers/hsd-server-starter/hsd-health-server/src/main/java/com/hsd/HealthAdminServer.java
@@ -0,0 +1,44 @@
+package com.hsd;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringCloudApplication
+@Configuration
+@EnableAdminServer
+
+@EnableTurbine
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
+
+@RestController
+public class HealthAdminServer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(HealthAdminServer.class, args);
+    }
+
+    @RequestMapping(value = "/hello")
+    @HystrixCommand
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("系统监控", HttpStatus.OK);
+    }
+}

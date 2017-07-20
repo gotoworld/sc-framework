5b8b3d8c5fbf8aa980769f2e0223a6b5362f4bec
wuxiaogang <wuxiaogang>
2017-07-20 17:51:38 +0800

    init

--- hsd-account/hsd-account-services/hsd-account-server/src/main/java/com/hsd/ServiceServer.java
+++ hsd-account/hsd-account-services/hsd-account-server/src/main/java/com/hsd/ServiceServer.java
@@ -1,17 +1,39 @@
 package com.hsd;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableFeignClients(basePackages = {"com.hsd"})
-@EnableDiscoveryClient
+
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
+
+@RestController
 public class ServiceServer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ServiceServer.class, args);
     }
+
+
+    @RequestMapping(value = "/hello")
+    @HystrixCommand
+    @ResponseBody
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("hello", HttpStatus.OK);
+    }
+
 }
--- hsd-account/hsd-account-web-boss/src/main/java/com/hsd/Application.java
+++ hsd-account/hsd-account-web-boss/src/main/java/com/hsd/Application.java
@@ -1,18 +1,30 @@
 package com.hsd;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @ServletComponentScan
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableFeignClients(basePackages = {"com.hsd"})
-//@EnableAutoConfiguration(exclude={MybatisAutoConfiguration.class})
+
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
+
+@RestController
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
@@ -22,4 +34,13 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @RequestMapping(value = "/hello")
+    @HystrixCommand
+    @ResponseBody
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("hello", HttpStatus.OK);
+    }
+
 }

96160f687163555b2d4d3ecab372155206ce2608
uname.chen <uname.chen@gmail.com>
2017-08-03 14:27:28 +0800

    模板与结构更新

--- hsd-servers/hsd-eycode-server/src/main/java/com/hsd/web/SchemaInfoController.java
+++ hsd-servers/hsd-eycode-server/src/main/java/com/hsd/web/SchemaInfoController.java
@@ -8,13 +8,9 @@ import com.hsd.service.ISchemaInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -117,8 +113,10 @@ public class SchemaInfoController extends BaseController {
                     schemaConf.set_update(request.getParameter(tableName+"_update"));//
                     schemaConf.set_page(request.getParameter(tableName+"_page"));//
                     schemaConf.set_list(request.getParameter(tableName+"_list"));//
+                    schemaConf.set_tree(request.getParameter(tableName+"_tree"));//
                     schemaConf.set_detail(request.getParameter(tableName+"_detail"));//
 
+                    schemaConf.set_my_project(request.getParameter("_my_project"));//
                     schemaConf.set_my_pkg(request.getParameter("_my_pkg"));//
                     schemaConf.set_domain_pkg(request.getParameter(tableName+"_domain_pkg"));//
                     schemaConf.set_dto_pkg(request.getParameter(tableName+"_dto_pkg"));//

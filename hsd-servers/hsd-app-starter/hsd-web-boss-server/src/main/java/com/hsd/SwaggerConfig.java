9a7a5da6aa52081d89d9084d446c77e39bddd8ff
uname.chen <uname.chen@gmail.com>
2017-08-04 17:55:04 +0800

    init

--- hsd-servers/hsd-app-starter/hsd-web-boss-server/src/main/java/com/hsd/SwaggerConfig.java
+++ hsd-servers/hsd-app-starter/hsd-web-boss-server/src/main/java/com/hsd/SwaggerConfig.java
@@ -2,17 +2,14 @@
 package com.hsd;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@Order(22)
-@EnableSwagger2
+//@Configuration
+//@Order(22)
+//@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
@@ -22,7 +19,7 @@ public class SwaggerConfig {
                 .forCodeGeneration(true)
                 .pathMapping("/")// api测试请求地址
                 .select()
-                .paths(PathSelectors.regex("/api/.*"))// 过滤的接口
+                .paths(PathSelectors.regex("/boss/.*"))// 过滤的接口
                 .build();
     }
 

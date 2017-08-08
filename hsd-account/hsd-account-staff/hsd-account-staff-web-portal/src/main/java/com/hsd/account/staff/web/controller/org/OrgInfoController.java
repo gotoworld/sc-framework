51625491c756d4d8a4c24e7b7c0b225556fc95b2
wuxiaogang <wuxiaogang>
2017-08-08 18:06:48 +0800

    init

--- hsd-account/hsd-account-staff/hsd-account-staff-web-portal/src/main/java/com/hsd/account/staff/web/controller/org/OrgInfoController.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-web-portal/src/main/java/com/hsd/account/staff/web/controller/org/OrgInfoController.java
@@ -23,7 +23,7 @@ import java.util.List;
 @RestController
 @Slf4j
 public class OrgInfoController extends BaseController {
-    private static final String acPrefix = "/api/account/org/";
+    private static final String acPrefix = "/api/account/staff/org/";
 
     @Autowired
     private IOrgInfoService orgDeptService;

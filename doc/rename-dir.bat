8634601fc93239514f1051249c33da07a1aad147
wuxiaogang <wuxiaogang>
2017-08-09 09:18:52 +0800

    init

--- doc/rename-dir.bat
+++ doc/rename-dir.bat
@@ -1,50 +1,37 @@
 @echo off
 setlocal enabledelayedexpansion
 
+
+set oldDirName=staff
+set oldDirLen=5
+set newDirName=channel
+
 set pa=%cd%
-
 call:findDir %pa%
-
-echo.&pause&goto:eof  
+echo.&pause&goto:eof
 echo on
-
-
 :findDir
 SETLOCAL
-
 set cur_dir=%1
 cd %cur_dir% 
 echo "搜索：" %cd%
-
 for /d  %%i in  (,*) do (
-	
 	if %%i NEQ "target" if %%i NEQ ".java" if  %%i NEQ ".xml" (
-
 		call:findDir %%i
-		
-		for /f "delims=" %%a in ('dir /b/s/a-d vr*.*') do (
+		for /f "delims=" %%a in ('dir /b/s/a-d %oldDirName%*.*') do (
 			set "str=%%~nxa"
-			ren "%%a" "hsd!str:~2!"
+			ren "%%a" "%newDirName%!str:~%oldDirLen%!"
 		)
-		
 		set m=%%i
-		set has_key=!m:vr=hsd!
-
+		set has_key=!m:%oldDirName%=hsd!
 		if !m! NEQ !has_key! (
 			echo "old->：" %cur_dir%
 			echo "new->：" %cd%
-			echo 重命名：!m! -》 !m:vr=hsd!
-
-			ren !m! !m:vr=hsd!
+			echo 重命名：!m! -》 !m:%oldDirName%=%newDirName%!
+			ren !m! !m:%oldDirName%=%newDirName%!
 		)
-		
 	)
-
 )
-
-
 if %cur_dir% NEQ %pa% cd ..\
-
-
 ENDLOCAL
 goto:eof

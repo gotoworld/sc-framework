9943f51df94612cd76335ec4d979b36f1969d44b
uname.chen <uname.chen@gmail.com>
2017-08-10 10:40:02 +0800

    init

--- hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/api/org/IOrgLogOperationService.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/api/org/IOrgLogOperationService.java
@@ -1,28 +1,33 @@
 package com.hsd.account.staff.api.org;
 
+import com.github.pagehelper.PageInfo;
 import com.hsd.account.staff.dto.org.OrgLogOperationDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 /**
  * <p>系统_管理员操作日志   业务处理接口类。
  */
 @FeignClient(name = "${feign.name.account.staff}")//, fallback = TestServiceHystrix.class)
 public interface IOrgLogOperationService {
-    String actPrefix = "/feign/account/staff/ISysUserLogService";
+    String acPrefix = "/feign/account/staff/ISysUserLogService";
 
     /**
      * 管理员操作日志记录。
      */
-    @RequestMapping(method = {RequestMethod.POST},value = actPrefix + "/info")
+    @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/info")
     public void info(@RequestParam("type") String type,@RequestParam("memo")  String memo,@RequestParam("domainCode")String domainCode,@RequestParam("detailInfo")  String detailInfo,@RequestParam("userId")  Long userId,@RequestParam("userName")  String userName,@RequestParam("ip")  String ip);
     /**
      * <p>信息列表 分页。
      */
-    @RequestMapping(method = {RequestMethod.POST},value = actPrefix + "/findDataIsPage")
-    public List<OrgLogOperationDto> findDataIsPage(OrgLogOperationDto dto);
+    @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findDataIsPage")
+    public PageInfo findDataIsPage(OrgLogOperationDto dto);
+
+    /**
+     * <p>信息详情。
+     */
+    @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findDataById")
+    public OrgLogOperationDto findDataById(OrgLogOperationDto dto) throws Exception;
 }

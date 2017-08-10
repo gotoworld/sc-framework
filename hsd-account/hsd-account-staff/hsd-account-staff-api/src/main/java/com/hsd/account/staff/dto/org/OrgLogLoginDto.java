f58a528ded06b09f2b4e08f1740cce0ed67af0f9
uname.chen <uname.chen@gmail.com>
2017-08-10 16:25:10 +0800

    init

--- hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/dto/org/OrgLogLoginDto.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/dto/org/OrgLogLoginDto.java
@@ -18,19 +18,14 @@ public class OrgLogLoginDto extends BaseDto {
      /**id*/
      private Long id;
      /**用户ID*/
-	 @NotNull(message="user_id不能为空")
      private Long userId;
      /**员工名称*/
-	 @NotNull(message="user_name不能为空")@Size(max=32,message="user_name最大32字符")
      private String userName;
      /**类型0登录1登出*/
-	 @NotNull(message="type不能为空")
      private Integer type;
      /**IP地址*/
-	 @NotNull(message="ip_addr不能为空")@Size(max=64,message="ip_addr最大64字符")
      private String ipAddr;
      /**MAC地址*/
-	 @NotNull(message="device_mac不能为空")@Size(max=32,message="device_mac最大32字符")
      private String deviceMac;
      /**创建时间*/
      private Date dateCreated;

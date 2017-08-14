5af52987b9fc0be926207d6764a9a553c886794e
uname.chen <uname.chen@gmail.com>
2017-08-14 13:44:38 +0800

    去除shiro 使用jwt完整替代

--- hsd-account/hsd-account-staff/hsd-account-staff-web-boss/src/main/java/com/hsd/account/staff/web/controller/org/OrgLogOperationController.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-web-boss/src/main/java/com/hsd/account/staff/web/controller/org/OrgLogOperationController.java
@@ -9,7 +9,7 @@ import com.hsd.web.controller.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
+import com.hsd.framework.annotation.auth.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 

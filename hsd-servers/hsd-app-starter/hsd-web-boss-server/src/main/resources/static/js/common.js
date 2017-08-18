12754ac39943ab66c28167752a7a0c21d0bbb249
uname.chen <uname.chen@gmail.com>
2017-08-18 12:34:20 +0800

    x

--- hsd-servers/hsd-app-starter/hsd-web-boss-server/src/main/resources/static/js/common.js
+++ hsd-servers/hsd-app-starter/hsd-web-boss-server/src/main/resources/static/js/common.js
@@ -235,6 +235,14 @@ var user = {
                 $data.closeMyBoxLayer = function () {
                     closeMyBoxLayer();
                 }
+                $data.kfReady=function(key,val){
+                    if(KindEditor) KindEditor.html('#'+key,val);
+                    return val;
+                }
+                $data.selected=function(val1,val2){
+                    if(val1==val2) return true
+                    return false;
+                }
                 $data.user = user;
             }
         }

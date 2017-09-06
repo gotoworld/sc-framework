705dee1768ceb104716c025377ab7fb545f40b5c
lilin <lilin@heshidai.com>
2017-09-06 15:25:51 +0800

    信息完善

--- hsd-account/hsd-account-channel/hsd-account-channel-web-portal/src/main/java/com/hsd/account/channel/web/controller/channel/ChannelInfoController.java
+++ hsd-account/hsd-account-channel/hsd-account-channel-web-portal/src/main/java/com/hsd/account/channel/web/controller/channel/ChannelInfoController.java
@@ -1,21 +1,37 @@
 package com.hsd.account.channel.web.controller.channel;
 
-import com.hsd.account.channel.api.channel.IChannelInfoService;
-import com.hsd.account.channel.dto.channel.ChannelInfoDto;
-import com.hsd.framework.Response;
-import com.hsd.framework.annotation.ALogOperation;
-import com.hsd.framework.annotation.RfAccount2Bean;
-import com.hsd.framework.web.controller.BaseController;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.hsd.account.channel.api.channel.IChannelInfoService;
+import com.hsd.account.channel.api.channel.IChannelTypeService;
+import com.hsd.account.channel.dto.channel.ChannelInfoDto;
+import com.hsd.account.channel.dto.channel.ChannelTypeDto;
+import com.hsd.framework.Response;
+import com.hsd.framework.annotation.ALogOperation;
+import com.hsd.framework.annotation.RfAccount2Bean;
+import com.hsd.framework.util.CommonConstant;
+import com.hsd.framework.web.controller.BaseController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
 @Api(description = "渠道商信息")
 @RestController
@@ -24,7 +40,9 @@ public class ChannelInfoController extends BaseController {
     private static final long serialVersionUID = -528422099490438672L;
     @Autowired
     private IChannelInfoService channelInfoService;
-    private static final String acPrefix = "/api/account/channel/channel/channelInfo/";
+    @Autowired
+    private IChannelTypeService channelTypeService;
+    private static final String acPrefix = "/api/account/channel/channelInfo/";
     /**
      * <p> 信息详情。
      */
@@ -73,6 +91,23 @@ public class ChannelInfoController extends BaseController {
                 result = channelInfoService.updataChannel(dto);
                 request.getSession().setAttribute(acPrefix + "edit." + dto.getToken(), "1");
             }
+        } catch (Exception e) {
+            result = Response.error(e.getMessage());
+        }
+        return result;
+    }
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = acPrefix + "list")
+    @ApiOperation(value = "渠道商类型列表")
+    public Response list() {
+        log.info("ChannelInfoController list.........");
+        Response result = new Response();
+        try {
+           
+        	ChannelTypeDto dto= new ChannelTypeDto();
+            dto.setPageSize(CommonConstant.PAGEROW_DEFAULT_COUNT);
+            dto.setDelFlag(0);
+            // 信息列表
+            result.data = channelTypeService.findDataIsList(dto);
         } catch (Exception e) {
             result = Response.error(e.getMessage());
         }

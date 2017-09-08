58751bea05aab31b1777020ae184b0fb3fba9ac7
wuxiaogang <wuxiaogang>
2017-09-08 15:52:46 +0800

    x

--- hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/user/UserSignContractController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/user/UserSignContractController.java
@@ -2,10 +2,8 @@ package com.hsd.actor.web.controller.user;
 
 import com.hsd.account.actor.api.user.IUserSignContractService;
 import com.hsd.account.actor.dto.user.UserSignContractDto;
-import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
 import com.hsd.framework.annotation.RfAccount2Bean;
-import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.web.controller.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -28,29 +26,6 @@ public class UserSignContractController extends BaseController {
     private static final String acPrefix = "/api/account/actor/user/userSignContract/";
 
     /**
-     * <p>信息分页 (未删除)。
-     */
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = acPrefix + "page/{pageNum}")
-    @ApiOperation(value = "信息分页")
-    public Response page(@ModelAttribute  UserSignContractDto dto, @PathVariable("pageNum") Integer pageNum) {
-        log.info("UserSignContractController page.........");
-        Response result = new Response();
-        try {
-            if (dto == null) {
-               dto = new UserSignContractDto();
-               dto.setPageSize(CommonConstant.PAGEROW_DEFAULT_COUNT);
-            }
-            dto.setPageNum(pageNum);
-
-            // 信息列表
-            result.data = PageUtil.copy(userSignContractService.findDataIsPage(dto));
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
-
-    /**
      * <p> 信息详情。
      */
     @RequestMapping(method = RequestMethod.GET, value = acPrefix + "info/{id}")
@@ -70,7 +45,6 @@ public class UserSignContractController extends BaseController {
         }
         return result;
     }
-
 
     /**
      * <p> 信息保存
--- hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/user/UserSnapshotController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/user/UserSnapshotController.java
@@ -2,10 +2,8 @@ package com.hsd.actor.web.controller.user;
 
 import com.hsd.account.actor.api.user.IUserSnapshotService;
 import com.hsd.account.actor.dto.user.UserSnapshotDto;
-import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
 import com.hsd.framework.annotation.RfAccount2Bean;
-import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.web.controller.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -28,31 +26,6 @@ public class UserSnapshotController extends BaseController {
     private static final String acPrefix = "/api/account/actor/user/userSnapshot/";
 
     /**
-     * <p>信息分页 (未删除)。
-     */
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = acPrefix + "page/{pageNum}")
-    @ApiOperation(value = "信息分页")
-    public Response page(@ModelAttribute  UserSnapshotDto dto, @PathVariable("pageNum") Integer pageNum) {
-        log.info("UserSnapshotController page.........");
-        Response result = new Response();
-        try {
-            if (dto == null) {
-               dto = new UserSnapshotDto();
-               dto.setPageSize(CommonConstant.PAGEROW_DEFAULT_COUNT);
-            }
-            dto.setPageNum(pageNum);
-
-            // 信息列表
-            result.data = PageUtil.copy(userSnapshotService.findDataIsPage(dto));
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
-
-
-
-    /**
      * <p> 信息详情。
      */
     @RequestMapping(method = RequestMethod.GET, value = acPrefix + "info/{userId}")
@@ -72,7 +45,6 @@ public class UserSnapshotController extends BaseController {
         }
         return result;
     }
-
 
     /**
      * <p> 信息保存

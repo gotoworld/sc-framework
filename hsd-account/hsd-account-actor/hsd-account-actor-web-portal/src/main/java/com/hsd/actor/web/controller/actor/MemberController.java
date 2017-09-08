7cd57cd77ee6116a5acac422d919d59f43a966d5
uname.chen <uname.chen@gmail.com>
2017-09-08 14:51:12 +0800

    x

--- hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/actor/MemberController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/actor/MemberController.java
@@ -2,14 +2,11 @@ package com.hsd.actor.web.controller.actor;
 
 import com.hsd.account.actor.api.actor.IMemberService;
 import com.hsd.account.actor.dto.actor.MemberDto;
+import com.hsd.account.actor.dto.user.UserDto;
 import com.hsd.account.actor.dto.user.UserExtInfoDto;
-import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
-import com.hsd.framework.annotation.ALogOperation;
 import com.hsd.framework.annotation.RfAccount2Bean;
-import com.hsd.framework.annotation.auth.Logical;
-import com.hsd.framework.annotation.auth.RequiresPermissions;
-import com.hsd.framework.util.CommonConstant;
+import com.hsd.framework.util.JwtUtil;
 import com.hsd.framework.util.StrUtil;
 import com.hsd.framework.util.ValidatorUtil;
 import com.hsd.framework.web.controller.BaseController;
@@ -20,7 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,65 +35,18 @@ public class MemberController extends BaseController {
     private static final String acPrefix = "/api/account/actor/actor/member/";
 
     /**
-     * <p>信息分页 (未删除)。
-     */
-    @RequiresPermissions("member:menu")
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = acPrefix + "page/{pageNum}")
-    @ApiOperation(value = "信息分页")
-    public Response page(@ModelAttribute MemberDto dto, @PathVariable("pageNum") Integer pageNum) {
-        log.info("MemberController page.........");
-        Response result = new Response();
-        try {
-            if (dto == null) {
-                dto = new MemberDto();
-                dto.setPageSize(CommonConstant.PAGEROW_DEFAULT_COUNT);
-            }
-            dto.setPageNum(pageNum);
-            dto.setDelFlag(0);
-            // 信息列表
-            result.data = PageUtil.copy(memberService.findDataIsPage(dto));
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
-
-
-    /**
      * <p> 信息详情。
      */
-    @RequiresPermissions("member:edit")
-    @RequestMapping(method = RequestMethod.GET, value = acPrefix + "info/{userId}")
+    @RequestMapping(method = RequestMethod.GET, value = acPrefix + "info")
     @ApiOperation(value = "信息详情")
-    public Response info(@PathVariable("userId") Long userId) {
+    public Response info() {
         log.info("MemberController info.........");
         Response result = new Response();
         try {
-            MemberDto dto = new MemberDto();
-            if (userId != null) {
-                dto.setUserId(userId);
-                dto.setDelFlag(0);
-                result.data = memberService.findDataById(dto);
-            }
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
-
-    /**
-     * <p>删除。
-     */
-    @RequiresPermissions("member:del")
-    @RequestMapping(method = RequestMethod.POST, value = acPrefix + "del/{userId}")
-    @ApiOperation(value = "信息删除")
-    public Response del(@PathVariable("userId") Long userId) {
-        log.info("MemberController del.........");
-        Response result = new Response();
-        try {
-            MemberDto dto = new MemberDto();
-            dto.setUserId(userId);
-            result.message = memberService.deleteDataById(dto);
+            result.data = memberService.findDataById(new MemberDto(){{
+                setUserId(JwtUtil.getSubject(UserDto.class).getId());
+                setDelFlag(0);
+            }});
         } catch (Exception e) {
             result = Response.error(e.getMessage());
         }
@@ -103,7 +56,6 @@ public class MemberController extends BaseController {
     /**
      * <p> 信息保存
      */
-    @RequiresPermissions(value = {"member:add", "member:edit"}, logical = Logical.OR)
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT}, value = acPrefix + "save")
     @ApiOperation(value = "信息保存")
     @RfAccount2Bean

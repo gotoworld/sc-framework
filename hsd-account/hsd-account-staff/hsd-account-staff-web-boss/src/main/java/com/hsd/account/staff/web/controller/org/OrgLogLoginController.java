36777114db33e7f786b4482f75fab450afb7e018
uname.chen <uname.chen@gmail.com>
2017-09-12 18:06:17 +0800

    x

--- hsd-account/hsd-account-staff/hsd-account-staff-web-boss/src/main/java/com/hsd/account/staff/web/controller/org/OrgLogLoginController.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-web-boss/src/main/java/com/hsd/account/staff/web/controller/org/OrgLogLoginController.java
@@ -4,22 +4,14 @@ import com.hsd.account.staff.api.org.IOrgLogLoginService;
 import com.hsd.account.staff.dto.org.OrgLogLoginDto;
 import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
-import com.hsd.framework.annotation.ALogOperation;
-import com.hsd.framework.annotation.RfAccount2Bean;
+import com.hsd.framework.annotation.auth.RequiresPermissions;
 import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.web.controller.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import com.hsd.framework.annotation.auth.Logical;
-import com.hsd.framework.annotation.auth.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Api(description = "组织架构_员工登录日志")
 @RestController
@@ -54,7 +46,6 @@ public class OrgLogLoginController extends BaseController {
     }
 
 
-
     /**
      * <p> 信息详情。
      */
@@ -69,40 +60,6 @@ public class OrgLogLoginController extends BaseController {
             if (id!=null) {
                 dto.setId(id);
                 result.data = orgLogLoginService.findDataById(dto);
-            }
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
-
-
-    /**
-     * <p> 信息保存
-     */
-    @RequiresPermissions(value = {"orgLogLogin:add", "orgLogLogin:edit"}, logical = Logical.OR)
-    @RequestMapping(method = {RequestMethod.POST,RequestMethod.PUT}, value = acPrefix + "save")
-    @RfAccount2Bean
-    @ALogOperation(type = "修改", desc = "组织架构_员工登录日志")
-    @ApiOperation(value = "信息保存")
-    public Response save(@Validated @ModelAttribute OrgLogLoginDto dto, BindingResult bindingResult) {
-        log.info("OrgLogLoginController save.........");
-        Response result = new Response();
-        try {
-            if (dto == null) return Response.error("参数获取异常!");
-            if ("1".equals(request.getSession().getAttribute(acPrefix + "save." + dto.getToken()))) {
-                throw new RuntimeException("请不要重复提交!");
-            }
-            if (bindingResult.hasErrors()) {
-                String errorMsg = "";
-                List<ObjectError> errorList = bindingResult.getAllErrors();
-                for (ObjectError error : errorList) {
-                    errorMsg += (error.getDefaultMessage()) + ";";
-                }
-                result = Response.error(errorMsg);
-            } else {
-                result = orgLogLoginService.saveOrUpdateData(dto);
-                request.getSession().setAttribute(acPrefix + "save." + dto.getToken(), "1");
             }
         } catch (Exception e) {
             result = Response.error(e.getMessage());

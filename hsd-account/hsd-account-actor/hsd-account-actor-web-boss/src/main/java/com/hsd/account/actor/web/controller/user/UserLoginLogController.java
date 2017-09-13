bb6c60f04f361f68ea102195adda2f9c866ccfc6
uname.chen <uname.chen@gmail.com>
2017-09-13 16:40:43 +0800

    x

--- hsd-account/hsd-account-actor/hsd-account-actor-web-boss/src/main/java/com/hsd/account/actor/web/controller/user/UserLoginLogController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-boss/src/main/java/com/hsd/account/actor/web/controller/user/UserLoginLogController.java
@@ -4,9 +4,6 @@ import com.hsd.account.actor.api.user.IUserLoginLogService;
 import com.hsd.account.actor.dto.user.UserLoginLogDto;
 import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
-import com.hsd.framework.annotation.ALogOperation;
-import com.hsd.framework.annotation.RfAccount2Bean;
-import com.hsd.framework.annotation.auth.Logical;
 import com.hsd.framework.annotation.auth.RequiresPermissions;
 import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.web.controller.BaseController;
@@ -14,12 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Api(description = "客户登录日志")
 @RestController
@@ -78,37 +70,4 @@ public class UserLoginLogController extends BaseController {
         return result;
     }
 
-
-    /**
-     * <p> 信息保存
-     */
-    @RequiresPermissions(value = {"userLoginLog:add", "userLoginLog:edit"}, logical = Logical.OR)
-    @RequestMapping(method = {RequestMethod.POST,RequestMethod.PUT}, value = acPrefix + "save")
-    @RfAccount2Bean
-    @ALogOperation(type = "修改", desc = "客户登录日志")
-    @ApiOperation(value = "信息保存")
-    public Response save(@Validated @ModelAttribute UserLoginLogDto dto, BindingResult bindingResult) {
-        log.info("UserrLoginLogController save.........");
-        Response result = new Response();
-        try {
-            if (dto == null) return Response.error("参数获取异常!");
-            if ("1".equals(request.getSession().getAttribute(acPrefix + "save." + dto.getToken()))) {
-                throw new RuntimeException("请不要重复提交!");
-            }
-            if (bindingResult.hasErrors()) {
-                String errorMsg = "";
-                List<ObjectError> errorList = bindingResult.getAllErrors();
-                for (ObjectError error : errorList) {
-                    errorMsg += (error.getDefaultMessage()) + ";";
-                }
-                result = Response.error(errorMsg);
-            } else {
-                result = userLoginLogService.saveOrUpdateData(dto);
-                request.getSession().setAttribute(acPrefix + "save." + dto.getToken(), "1");
-            }
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
 }
--- hsd-account/hsd-account-actor/hsd-account-actor-web-boss/src/main/java/com/hsd/account/actor/web/controller/user/UserSignContractController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-boss/src/main/java/com/hsd/account/actor/web/controller/user/UserSignContractController.java
@@ -4,9 +4,6 @@ import com.hsd.account.actor.api.user.IUserSignContractService;
 import com.hsd.account.actor.dto.user.UserSignContractDto;
 import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
-import com.hsd.framework.annotation.ALogOperation;
-import com.hsd.framework.annotation.RfAccount2Bean;
-import com.hsd.framework.annotation.auth.Logical;
 import com.hsd.framework.annotation.auth.RequiresPermissions;
 import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.web.controller.BaseController;
@@ -14,12 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Api(description = "客户网签协议记录")
 @RestController
@@ -78,37 +70,4 @@ public class UserSignContractController extends BaseController {
         return result;
     }
 
-
-    /**
-     * <p> 信息保存
-     */
-    @RequiresPermissions(value = {"userSignContract:add", "userSignContract:edit"}, logical = Logical.OR)
-    @RequestMapping(method = {RequestMethod.POST,RequestMethod.PUT}, value = acPrefix + "save")
-    @RfAccount2Bean
-    @ALogOperation(type = "修改", desc = "客户网签协议记录")
-    @ApiOperation(value = "信息保存")
-    public Response save(@Validated @ModelAttribute UserSignContractDto dto, BindingResult bindingResult) {
-        log.info("UserSignContractController save.........");
-        Response result = new Response();
-        try {
-            if (dto == null) return Response.error("参数获取异常!");
-            if ("1".equals(request.getSession().getAttribute(acPrefix + "save." + dto.getToken()))) {
-                throw new RuntimeException("请不要重复提交!");
-            }
-            if (bindingResult.hasErrors()) {
-                String errorMsg = "";
-                List<ObjectError> errorList = bindingResult.getAllErrors();
-                for (ObjectError error : errorList) {
-                    errorMsg += (error.getDefaultMessage()) + ";";
-                }
-                result = Response.error(errorMsg);
-            } else {
-                result = userSignContractService.saveOrUpdateData(dto);
-                request.getSession().setAttribute(acPrefix + "save." + dto.getToken(), "1");
-            }
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
 }

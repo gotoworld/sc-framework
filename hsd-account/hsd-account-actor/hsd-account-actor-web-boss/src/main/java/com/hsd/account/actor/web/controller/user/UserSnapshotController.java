49f3a4e0b057a6d2bf86faacea0edaacc31e82dd
wuxiaogang <wuxiaogang>
2017-09-13 16:40:43 +0800

    x

--- hsd-account/hsd-account-actor/hsd-account-actor-web-boss/src/main/java/com/hsd/account/actor/web/controller/user/UserSnapshotController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-boss/src/main/java/com/hsd/account/actor/web/controller/user/UserSnapshotController.java
@@ -2,24 +2,17 @@ package com.hsd.account.actor.web.controller.user;
 
 import com.hsd.account.actor.api.user.IUserSnapshotService;
 import com.hsd.account.actor.dto.user.UserSnapshotDto;
-import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
-import com.hsd.framework.annotation.ALogOperation;
-import com.hsd.framework.annotation.RfAccount2Bean;
-import com.hsd.framework.annotation.auth.Logical;
 import com.hsd.framework.annotation.auth.RequiresPermissions;
-import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.web.controller.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(description = "客户照表")
 @RestController
@@ -29,31 +22,6 @@ public class UserSnapshotController extends BaseController {
     @Autowired
     private IUserSnapshotService userSnapshotService;
     private static final String acPrefix = "/boss/account/actor/user/userSnapshot/";
-
-    /**
-     * <p>信息分页 (未删除)。
-     */
-    @RequiresPermissions("userSnapshot:menu")
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = acPrefix + "page/{pageNum}")
-    @ApiOperation(value = "信息分页")
-    public Response page(@ModelAttribute  UserSnapshotDto dto, @PathVariable("pageNum") Integer pageNum) {
-        log.info("UserSnapshotController page.........");
-        Response result = new Response();
-        try {
-            if (dto == null) {
-               dto = new UserSnapshotDto();
-               dto.setPageSize(CommonConstant.PAGEROW_DEFAULT_COUNT);
-            }
-            dto.setPageNum(pageNum);
-
-            // 信息列表
-            result.data = PageUtil.copy(userSnapshotService.findDataIsPage(dto));
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
-
 
 
     /**
@@ -78,37 +46,4 @@ public class UserSnapshotController extends BaseController {
         return result;
     }
 
-
-    /**
-     * <p> 信息保存
-     */
-    @RequiresPermissions(value = {"userSnapshot:add", "userSnapshot:edit"}, logical = Logical.OR)
-    @RequestMapping(method = {RequestMethod.POST,RequestMethod.PUT}, value = acPrefix + "save")
-    @RfAccount2Bean
-    @ALogOperation(type = "修改", desc = "客户照表")
-    @ApiOperation(value = "信息保存")
-    public Response save(@Validated @ModelAttribute UserSnapshotDto dto, BindingResult bindingResult) {
-        log.info("UserSnapshotController save.........");
-        Response result = new Response();
-        try {
-            if (dto == null) return Response.error("参数获取异常!");
-            if ("1".equals(request.getSession().getAttribute(acPrefix + "save." + dto.getToken()))) {
-                throw new RuntimeException("请不要重复提交!");
-            }
-            if (bindingResult.hasErrors()) {
-                String errorMsg = "";
-                List<ObjectError> errorList = bindingResult.getAllErrors();
-                for (ObjectError error : errorList) {
-                    errorMsg += (error.getDefaultMessage()) + ";";
-                }
-                result = Response.error(errorMsg);
-            } else {
-                result = userSnapshotService.saveOrUpdateData(dto);
-                request.getSession().setAttribute(acPrefix + "save." + dto.getToken(), "1");
-            }
-        } catch (Exception e) {
-            result = Response.error(e.getMessage());
-        }
-        return result;
-    }
 }

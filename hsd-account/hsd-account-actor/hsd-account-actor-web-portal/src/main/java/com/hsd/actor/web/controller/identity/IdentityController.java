49f8fa750bc296df820eddfdc80a3094e7b68653
uname.chen <uname.chen@gmail.com>
2017-09-14 11:40:58 +0800

    x

--- hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/identity/IdentityController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/identity/IdentityController.java
@@ -2,18 +2,19 @@ package com.hsd.actor.web.controller.identity;
 
 import com.hsd.account.actor.api.identity.IIdentityService;
 import com.hsd.account.actor.dto.identity.IdentityDto;
+import com.hsd.account.actor.dto.user.UserDto;
 import com.hsd.framework.Response;
+import com.hsd.framework.util.JwtUtil;
 import com.hsd.framework.web.controller.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(description = "实名认证表")
+@Api(description = "实名认证")
 @RestController
 @Slf4j
 public class IdentityController extends BaseController {
@@ -25,16 +26,14 @@ public class IdentityController extends BaseController {
     /**
      * <p> 信息详情。
      */
-    @RequestMapping(method = RequestMethod.GET, value = acPrefix + "info/{userId}")
+    @RequestMapping(method = RequestMethod.GET, value = acPrefix + "info")
     @ApiOperation(value = "信息详情")
-    public Response info(@PathVariable("userId") Long userId) {
+    public Response info() {
         log.info("IdentityController info.........");
         Response result = new Response();
         try {
-            if (userId==null) {throw new RuntimeException("参数异常!");};
             IdentityDto dto = new IdentityDto(){{
-                setUserId(userId);
-
+                setUserId(JwtUtil.getSubject(UserDto.class).getId());
             }};
             result.data = identityService.findDataById(dto);
         } catch (Exception e) {

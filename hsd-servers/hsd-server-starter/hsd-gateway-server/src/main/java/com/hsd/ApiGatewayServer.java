2957d77ccb496e392c9c756e4b8b4d398a23596c
wuxiaogang <wuxiaogang>
2017-09-20 11:40:12 +0800

    x

--- hsd-servers/hsd-server-starter/hsd-gateway-server/src/main/java/com/hsd/ApiGatewayServer.java
+++ hsd-servers/hsd-server-starter/hsd-gateway-server/src/main/java/com/hsd/ApiGatewayServer.java
@@ -5,6 +5,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -13,5 +17,11 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ApiGatewayServer {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ApiGatewayServer.class).web(true).run(args);
+    }
+
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("网关访问方式http://${网关地址}:${网关端口}/${服务名}/", HttpStatus.OK);
     }
 }

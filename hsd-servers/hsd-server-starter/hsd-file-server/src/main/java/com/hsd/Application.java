e27639edae4fbc189c289f57f6d0d0fe39d658e7
wuxiaogang <wuxiaogang>
2017-09-22 16:43:06 +0800

    x

--- hsd-servers/hsd-server-starter/hsd-file-server/src/main/java/com/hsd/Application.java
+++ hsd-servers/hsd-server-starter/hsd-file-server/src/main/java/com/hsd/Application.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -22,9 +19,8 @@ public class Application extends SpringBootServletInitializer {
         SpringApplication.run(Application.class, args);
     }
     @RequestMapping(value = "/")
-    @ResponseBody
-    public ResponseEntity<String> hello() {
-        return new ResponseEntity<String>("文件上传服务", HttpStatus.OK);
+    public String hello() {
+        return "hello! 文件上传服务";
     }
 
 }

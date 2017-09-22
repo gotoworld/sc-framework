6b051e2d0e86d4e69ea232def0b06539d75a0857
uname.chen <uname.chen@gmail.com>
2017-09-22 16:41:06 +0800

    x

--- hsd-servers/hsd-server-starter/hsd-file-server/src/main/java/com/hsd/Application.java
+++ hsd-servers/hsd-server-starter/hsd-file-server/src/main/java/com/hsd/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,6 +20,11 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("文件上传服务", HttpStatus.OK);
     }
 
 }

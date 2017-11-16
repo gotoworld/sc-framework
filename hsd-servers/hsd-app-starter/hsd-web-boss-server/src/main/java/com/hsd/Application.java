870e848dd2dd9e2ac167123095d9492706abc99f
uname.chen <uname.chen@gmail.com>
2017-11-16 14:10:42 +0800

    配置文件微调

--- hsd-servers/hsd-app-starter/hsd-web-boss-server/src/main/java/com/hsd/Application.java
+++ hsd-servers/hsd-app-starter/hsd-web-boss-server/src/main/java/com/hsd/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @ServletComponentScan
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
 public class Application extends SpringBootServletInitializer {

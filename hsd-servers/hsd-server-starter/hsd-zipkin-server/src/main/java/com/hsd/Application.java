288ac0b77ece5a9aff468c65b19a0a6ecf01a383
uname.chen <uname.chen@gmail.com>
2017-11-16 11:37:36 +0800

    配置文件微调

---
+++ hsd-servers/hsd-server-starter/hsd-zipkin-server/src/main/java/com/hsd/Application.java
@@ -0,0 +1,25 @@
+package com.hsd;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("服务链路追踪", HttpStatus.OK);
+    }
+}

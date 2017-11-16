908fc0b32710de5ac52d096b1c75ded86601ad84
wuxiaogang <wuxiaogang>
2017-11-16 14:10:42 +0800

    配置文件微调

--- hsd-servers/hsd-server-starter/hsd-zipkin-server/src/main/java/com/hsd/Application.java
+++ hsd-servers/hsd-server-starter/hsd-zipkin-server/src/main/java/com/hsd/Application.java
@@ -2,6 +2,7 @@ package com.hsd;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableZipkinServer
 @RestController
 public class Application {

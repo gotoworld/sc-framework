954d748be90b48e7ebc45f35f69202dfd85692c5
wuxiaogang <wuxiaogang>
2017-11-17 11:49:55 +0800

    配置文件调整  分布式配置分发

--- hsd-servers/hsd-app-starter/hsd-service-server/src/main/java/com/hsd/ServiceServer.java
+++ hsd-servers/hsd-app-starter/hsd-service-server/src/main/java/com/hsd/ServiceServer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class ServiceServer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ServiceServer.class, args);

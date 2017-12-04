ade717cb1926da4fb060b66327696318fe62bfa8
uname.chen <uname.chen@gmail.com>
2017-12-04 14:35:47 +0800

    app用户

---
+++ hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/dto/sys/UserAppDto.java
@@ -0,0 +1,32 @@
+package com.hsd.account.staff.dto.sys;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.hsd.framework.dto.BaseDto;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel("APP用户表 DTO")
+public class UserAppDto extends BaseDto {
+     @ApiModelProperty("app用户id")
+     private Long id;
+     @ApiModelProperty("用户id")
+     private Long userId;
+     @ApiModelProperty("应用id")
+	 @Size(max=22,message="app_id最大22字符")
+     private String appId;
+     @ApiModelProperty("注册时间")
+     private Date dateCreated;
+     @ApiModelProperty("BI时间戳")
+     private Date biUpdateTs;
+
+}

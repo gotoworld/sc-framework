8ac2c391c9abbb1416949ce3503eae0feafd50d4
uname.chen <uname.chen@gmail.com>
2017-12-05 17:11:19 +0800

    授权调整/网关过滤

--- hsd-servers/hsd-server-starter/hsd-gateway-server/src/main/java/com/hsd/config/FilterConfig.java
+++ hsd-servers/hsd-server-starter/hsd-gateway-server/src/main/java/com/hsd/config/FilterConfig.java
@@ -1,13 +1,17 @@
 package com.hsd.config;
 
 import com.hsd.filter.CrossFilter;
+import com.hsd.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig {
     @Bean
-    public CrossFilter headerFilter() {
+    public CrossFilter crossFilter() {
         return new CrossFilter();
     }
+
+    @Bean
+    public JwtFilter jwtFilter() { return new JwtFilter(); }
 }

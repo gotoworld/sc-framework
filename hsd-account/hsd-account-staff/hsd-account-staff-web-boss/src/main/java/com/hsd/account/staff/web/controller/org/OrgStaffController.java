deed047dcd56f65b0c9b6d48ccf53c2825ad1fb1
chenchen <1406743115@qq.com>
2017-12-06 11:41:52 +0800

    获取最大员工号
    Signed-off-by: chenchen <1406743115@qq.com>

--- hsd-account/hsd-account-staff/hsd-account-staff-web-boss/src/main/java/com/hsd/account/staff/web/controller/org/OrgStaffController.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-web-boss/src/main/java/com/hsd/account/staff/web/controller/org/OrgStaffController.java
@@ -29,7 +29,7 @@ import java.util.List;
 @Api(description = "组织架构_员工")
 @RestController
 @Slf4j
-@NoAuthorize
+
 public class OrgStaffController extends BaseController {
 	private static final String acPrefix="/boss/account/staff/org/orgStaff/";
 
@@ -465,7 +465,6 @@ public class OrgStaffController extends BaseController {
 		}
 		return result;
 	}
-	//@RequiresPermissions("orgStaff:menu")
 	@RequestMapping(method={RequestMethod.GET,RequestMethod.POST},value = acPrefix + "maxJobNo")
 	@ApiOperation(value = "获取不同类型最大的员工号")
 	public Response getMaxJobNo() {

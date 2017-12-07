e97ac0f5c2052035e18fdc870bc87ef7034ae5e1
wuxiaogang <wuxiaogang>
2017-12-07 16:49:46 +0800

    bug fix

--- hsd-framework-starter/hsd-framework-api/src/main/java/com/hsd/framework/exception/FeignServiceException.java
+++ hsd-framework-starter/hsd-framework-api/src/main/java/com/hsd/framework/exception/FeignServiceException.java
@@ -1,29 +1,26 @@
-/**
- *
- */
 package com.hsd.framework.exception;
 
 /**
  * @Description: 异常基类
  */
-public class ServiceFeignException extends RuntimeException  {
+public class FeignServiceException extends RuntimeException  {
     private static final long serialVersionUID = 5692243177785821696L;
 
     private int error ;
     private Object data = "";
 
-    public ServiceFeignException(int error) {
+    public FeignServiceException(int error) {
         this(error, "", "");
     }
 
-    public ServiceFeignException(int error, String msg) {
+    public FeignServiceException(int error, String msg) {
         this(error, msg, "");
     }
 
-    public ServiceFeignException(int error, String msg, Object data) {
+    public FeignServiceException(int error, String msg, Object data) {
         super(msg );
-    	this.error = error;
-    	this.data = data;
+        this.error = error;
+        this.data = data;
     }
 
     public int getError() {

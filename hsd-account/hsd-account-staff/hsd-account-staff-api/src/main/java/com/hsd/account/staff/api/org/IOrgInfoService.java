2ebd091cbcd94f1e86e9004b1f1123604cbc9742
uname.chen <uname.chen@gmail.com>
2017-12-07 17:34:18 +0800

    bug fix

--- hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/api/org/IOrgInfoService.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/api/org/IOrgInfoService.java
@@ -26,100 +26,102 @@ public interface IOrgInfoService {
      * <p>信息编辑。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/saveOrUpdateData")
-    public Response saveOrUpdateData(OrgInfoDto dto) throws Exception;
+    Response saveOrUpdateData(OrgInfoDto dto) throws Exception;
     /**
      * <p>物理删除。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/deleteData")
-    public String deleteData(OrgInfoDto dto) throws Exception;
+    String deleteData(OrgInfoDto dto) throws Exception;
 
     /**
      * <p>恢复逻辑删除的数据 单条。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/recoveryDataById")
-    public String recoveryDataById(OrgInfoDto dto) throws Exception;
+    String recoveryDataById(OrgInfoDto dto) throws Exception;
 
     /**
      * <p>逻辑删除 单条。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/deleteDataById")
-    public String deleteDataById(OrgInfoDto dto) throws Exception;
+    String deleteDataById(OrgInfoDto dto) throws Exception;
 
     /**
      * <p>信息列表 分页。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findDataIsPage")
-    public PageInfo findDataIsPage(OrgInfoDto dto) throws Exception;
+    PageInfo findDataIsPage(OrgInfoDto dto) throws Exception;
     /**
      * <p>信息列表 分页。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findBriefDataIsPage")
-    public PageInfo findBriefDataIsPage(OrgInfoDto dto) throws Exception;
+    PageInfo findBriefDataIsPage(OrgInfoDto dto) throws Exception;
 
     /**
      * <p>信息列表。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findDataIsList")
-    public List<OrgInfoDto> findDataIsList(OrgInfoDto dto);
+    List<OrgInfoDto> findDataIsList(OrgInfoDto dto);
 
     /**
      * <p>信息详情。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findDataById")
-    public OrgInfoDto findDataById(OrgInfoDto dto);
+    OrgInfoDto findDataById(OrgInfoDto dto);
     /**
      * <p>信息详情。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findDataByCode")
-    public OrgInfoDto findDataByCode(OrgInfoDto dto);
+    OrgInfoDto findDataByCode(OrgInfoDto dto);
 
     /**
      * <p>信息树。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findDataIsTree")
-    public List<OrgInfoDto> findDataIsTree(OrgInfoDto dto);
+    List<OrgInfoDto> findDataIsTree(OrgInfoDto dto);
 
 
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findStaffByIdIsList")
-    public List<OrgStaffDto> findOrgStaffIsList(OrgOrgVsStaffDto dto);
+    List<OrgStaffDto> findOrgStaffIsList(OrgOrgVsStaffDto dto);
     /**
      * <p>添加人员。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/addStaff")
-    public Response addStaff(OrgOrgVsStaffDto dto) throws Exception;
+    Response addStaff(OrgOrgVsStaffDto dto) throws Exception;
 
     /**
      * <p>删除人员。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/delStaff")
-    public Response delStaff(OrgOrgVsStaffDto dto) throws Exception;
+    Response delStaff(OrgOrgVsStaffDto dto) throws Exception;
 
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findOrgRoleIsList")
-    public List<AuthRoleDto> findOrgRoleIsList(OrgInfoDto dto);
+    List<AuthRoleDto> findOrgRoleIsList(OrgInfoDto dto);
 
     /**
      * <p>添加角色。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/addRole")
-    public Response addRole(OrgOrgVsRoleDto dto) throws Exception;
+    Response addRole(OrgOrgVsRoleDto dto) throws Exception;
     /**
      * <p>删除角色。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/delRole")
-    public Response delRole(OrgOrgVsRoleDto dto) throws Exception;
+    Response delRole(OrgOrgVsRoleDto dto) throws Exception;
     /**
      * <p>设置部门负责人。
      */
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/setManager")
-    public Response setManager(OrgInfoDto dto) throws Exception;
+    Response setManager(OrgInfoDto dto) throws Exception;
+    @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/getManager")
+    Response getManager(OrgInfoDto dto) throws Exception;
 
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findOrgStaffByCodeIsList")
-    public List<OrgStaffDto> findOrgStaffByCodeIsList(OrgOrgVsStaffDto dto);
+    List<OrgStaffDto> findOrgStaffByCodeIsList(OrgOrgVsStaffDto dto);
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findOrgLevel")
-    public String findOrgLevel(OrgOrgVsStaffDto dto);
+    String findOrgLevel(OrgOrgVsStaffDto dto);
 
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findChildDataIsList")
-    public List<OrgInfoDto> findChildDataIsList(OrgInfoDto dto);
+    List<OrgInfoDto> findChildDataIsList(OrgInfoDto dto);
     @RequestMapping(method = {RequestMethod.POST},value = acPrefix + "/findOrgChildStaffIsList")
-    public List<OrgInfoDto> findOrgChildStaffIsList(OrgInfoDto dto);
+    List<OrgInfoDto> findOrgChildStaffIsList(OrgInfoDto dto);
 }

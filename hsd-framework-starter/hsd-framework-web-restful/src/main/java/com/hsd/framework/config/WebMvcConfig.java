6fa5306ffbf2cd3d018a958cc2e41255182c937e
wuxiaogang <wuxiaogang>
2017-12-08 17:19:00 +0800

    解决js处理Long类型丢失精度的问题

--- hsd-framework-starter/hsd-framework-web-restful/src/main/java/com/hsd/framework/config/WebMvcConfig.java
+++ hsd-framework-starter/hsd-framework-web-restful/src/main/java/com/hsd/framework/config/WebMvcConfig.java
@@ -1,5 +1,9 @@
 package com.hsd.framework.config;
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.hsd.framework.interceptor.JwtInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
@@ -8,8 +12,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @Configuration
 @Slf4j
@@ -28,5 +36,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addInterceptor(new JwtInterceptor()).addPathPatterns("/api/**","/boss/**");//权限验证拦截器
 
         super.addInterceptors(registry);
+    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(serializeConfig);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(fastConverter);
     }
 }

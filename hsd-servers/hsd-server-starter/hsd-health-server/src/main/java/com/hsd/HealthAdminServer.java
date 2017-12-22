47e6b98b9320a0f66bd26666fb5512e2969d8fd5
uname.chen <uname.chen@gmail.com>
2017-12-22 14:25:07 +0800

    bug  fix

--- hsd-servers/hsd-server-starter/hsd-health-server/src/main/java/com/hsd/HealthAdminServer.java
+++ hsd-servers/hsd-server-starter/hsd-health-server/src/main/java/com/hsd/HealthAdminServer.java
@@ -3,6 +3,7 @@ package com.hsd;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.SpringCloudApplication;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringCloudApplication
 @Configuration
+@EnableAutoConfiguration
 @EnableAdminServer
 
 @EnableTurbine

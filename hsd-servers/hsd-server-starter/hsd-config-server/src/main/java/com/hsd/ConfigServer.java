8bd1d7ab48b27fde199564d78dbaea57908fef6d
uname.chen <uname.chen@gmail.com>
2017-12-26 11:00:41 +0800

    yml x

--- hsd-servers/hsd-server-starter/hsd-config-server/src/main/java/com/hsd/ConfigServer.java
+++ hsd-servers/hsd-server-starter/hsd-config-server/src/main/java/com/hsd/ConfigServer.java
@@ -1,5 +1,6 @@
 package com.hsd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,9 +9,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@Slf4j
 public class ConfigServer {
 
     public static void main(String[] args) {
+        log.info("ConfigServer 启动.....");
         SpringApplication.run(ConfigServer.class, args);
     }
 }

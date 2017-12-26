b12d787be3e1afa0709f9fdc4ff15aec479de121
uname.chen <uname.chen@gmail.com>
2017-12-26 15:59:01 +0800

    yml x

--- hsd-servers/hsd-server-starter/hsd-gateway-server/src/main/java/com/hsd/ApiGatewayServer.java
+++ hsd-servers/hsd-server-starter/hsd-gateway-server/src/main/java/com/hsd/ApiGatewayServer.java
@@ -1,5 +1,8 @@
 package com.hsd;
 
+import com.hsd.framework.config.AppConfig;
+import com.hsd.framework.security.AES;
+import com.hsd.framework.util.ReflectUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -8,6 +11,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +30,11 @@ public class ApiGatewayServer {
     @RequestMapping(value = "/")
     @ResponseBody
     public ResponseEntity<String> hello() {
-        return new ResponseEntity<String>("Hi! 网关访问方式=>http://${网关地址}:${网关端口}/${服务名}/", HttpStatus.OK);
+        return new ResponseEntity<String>("Hi! 网关访问方式=>http://:${网关端口}/${服务名}/", HttpStatus.OK);
+    }
+    @RequestMapping(value = "/aes/{str}")
+    @ResponseBody
+    public ResponseEntity<String> aes(@PathVariable("str") String str) {
+        return new ResponseEntity<String>("${AES:XXX-"+AES.encrypt(str, ReflectUtil.getValueByFieldName(new AppConfig(),"prikey") +"-XXX}"), HttpStatus.OK);
     }
 }

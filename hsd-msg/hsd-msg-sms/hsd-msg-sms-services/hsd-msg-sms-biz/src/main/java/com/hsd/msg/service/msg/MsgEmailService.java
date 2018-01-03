a6697cab1dca700ffc8b746da3810080c5f51f7c
uname.chen <uname.chen@gmail.com>
2018-01-03 15:28:30 +0800

    合理性调整

---
+++ hsd-msg/hsd-msg-sms/hsd-msg-sms-services/hsd-msg-sms-biz/src/main/java/com/hsd/msg/service/msg/MsgEmailService.java
@@ -0,0 +1,57 @@
+package com.hsd.msg.service.msg;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.hsd.framework.SysErrorCode;
+import com.hsd.framework.annotation.FeignService;
+import com.hsd.framework.exception.ServiceException;
+import com.hsd.framework.service.BaseService;
+import com.hsd.msg.api.msg.IMsgEmailService;
+import com.hsd.msg.dao.msg.IMsgEmailDao;
+import com.hsd.msg.dto.msg.MsgEmailDto;
+import com.hsd.msg.entity.msg.MsgEmail;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignService
+@Slf4j
+public class MsgEmailService extends BaseService implements IMsgEmailService {
+    @Autowired
+    private IMsgEmailDao msgEmailDao;
+
+    @Override
+    public PageInfo findDataIsPage(@RequestBody MsgEmailDto dto) {
+        PageInfo pageInfo = null;
+        try {
+            if (dto == null) throw new RuntimeException("参数异常!");
+            MsgEmail entity = copyTo(dto, MsgEmail.class);
+            PageHelper.startPage(PN(dto.getPageNum()), PS(dto.getPageSize()));
+            List list = msgEmailDao.findDataIsPage(entity);
+            pageInfo = new PageInfo(list);
+            pageInfo.setList(copyTo(pageInfo.getList(), MsgEmailDto.class));
+        } catch (Exception e) {
+            log.error("信息[分页]查询异常!", e);
+            throw new ServiceException(SysErrorCode.defaultError, e.getMessage());
+        }
+        return pageInfo;
+    }
+
+
+    @Override
+    public MsgEmailDto findDataById(@RequestBody MsgEmailDto dto) {
+        MsgEmailDto result = null;
+        try {
+            MsgEmail entity = copyTo(dto, MsgEmail.class);
+            result = copyTo(msgEmailDao.selectByPrimaryKey(entity), MsgEmailDto.class);
+        } catch (Exception e) {
+            log.error("信息[详情]查询异常!", e);
+            throw new ServiceException(SysErrorCode.defaultError, e.getMessage());
+        }
+        return result;
+    }
+
+
+}

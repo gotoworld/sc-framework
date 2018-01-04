7bdbcd2da4d1ec5283fa7904f504fb0defc3c179
uname.chen <uname.chen@gmail.com>
2018-01-04 15:45:40 +0800

    综合账户功能编码

---
+++ hsd-account/hsd-account-actor/hsd-account-actor-services/hsd-account-actor-dao/src/main/java/com/hsd/account/actor/dao/user/IUserAppDao.java
@@ -0,0 +1,21 @@
+package com.hsd.account.actor.dao.user;
+
+import com.hsd.framework.IBaseDao;
+import com.hsd.framework.IEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * <p>APP用户表 数据库处理接口类。
+ */
+@Mapper
+public interface IUserAppDao extends IBaseDao {
+
+    /**
+     * 判断是否存在
+     */
+    @Select("select IFNULL(count(0),0) as count from user_app where  ")
+    int isDataYN(IEntity entity) throws Exception;
+
+    Object findDataByAppIdAndUserId(IEntity entity) throws Exception;
+}

e640cfd0b3a23b7002b40f50bd2c5722b35c933e
uname.chen <uname.chen@gmail.com>
2018-01-04 15:45:40 +0800

    综合账户功能编码

--- hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/LoginController.java
+++ hsd-account/hsd-account-actor/hsd-account-actor-web-portal/src/main/java/com/hsd/actor/web/controller/LoginController.java
@@ -2,13 +2,18 @@ package com.hsd.actor.web.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.hsd.account.actor.api.identity.IIdentityService;
+import com.hsd.account.actor.api.user.IUserAppService;
 import com.hsd.account.actor.api.user.IUserLoginLogService;
 import com.hsd.account.actor.api.user.IUserService;
 import com.hsd.account.actor.dto.identity.IdentityDto;
+import com.hsd.account.actor.dto.user.UserAppDto;
 import com.hsd.account.actor.dto.user.UserDto;
 import com.hsd.account.actor.dto.user.UserLoginLogDto;
+import com.hsd.account.staff.api.sys.ISysAppService;
+import com.hsd.account.staff.dto.sys.SysAppDto;
 import com.hsd.framework.Response;
 import com.hsd.framework.annotation.NoAuthorize;
+import com.hsd.framework.cache.util.RedisHelper;
 import com.hsd.framework.security.MD5;
 import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.util.IpUtil;
@@ -27,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>登录登出action
@@ -43,53 +49,70 @@ public class LoginController extends BaseController {
     private IUserLoginLogService loginLogService;
     @Autowired
     private IIdentityService identityService;
-
+    @Autowired
+    private ISysAppService sysAppService;
+    @Autowired
+    private IUserAppService userAppService;
+    @Autowired
+    private RedisHelper redisHelper;
     /**
      * <p>用户登录
      */
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = acPrefix + "/login")
     @ApiOperation(value = "登录")
-    public Response login(@RequestParam("account") String account, @RequestParam("password") String password) throws Exception {
+    public Response login(@RequestParam("account") String account, @RequestParam("password") String password,@RequestParam("appId") String appId) {
         log.info("LoginController login");
         Response result = new Response();
         try {
             if (ValidatorUtil.isNullEmpty(account) || ValidatorUtil.isNullEmpty(password)) {
                 return Response.error("用户名或密码不能为空!");
             }
-            UserDto user = userService.findUserByAccount(account, UserDto.userType.USER.getCode());
+            SysAppDto sysAppDto = sysAppService.findDataById(new SysAppDto(){{setId(appId);}});
+            if (sysAppDto == null){
+                return  Response.error("应用系统不存在!");
+            }
+            UserDto userDto = userService.findUserByAccount(account, UserDto.userType.USER.getCode());
             String pwdhex = MD5.pwdMd5Hex(password);
-            if (user == null || !(account.equals(user.getAccount()) && pwdhex.equals(user.getPwd()))) {
+            if (userDto == null || !(account.equals(userDto.getAccount()) && pwdhex.equals(userDto.getPwd()))) {
                 return Response.error("登录失败,用户名或密码错误!");
             }
-            userService.lastLogin(user);
+            userService.lastLogin(userDto);
 
-            String subject = JwtUtil.generalSubject(user);
+
+
+            //查询APP员工表
+            UserAppDto userAppDto = userAppService.findDataByAppIdAndUserId(new UserAppDto(){{setUserId(userDto.getId());setAppId(sysAppDto.getId());}});
+            userDto.setAppUserId(userAppDto.getId());
+            userDto.setAppId(sysAppDto.getId());
+            userDto.setAppName(sysAppDto.getName());
+
+            String subject = JwtUtil.generalSubject(userDto);
             String authorizationToken = JwtUtil.createJWT(JwtUtil.UserType.USER, CommonConstant.JWT_ID, subject, CommonConstant.JWT_TTL);
 
             Map<String, Object> data = new HashMap<>();
             data.put("tokenExpMillis", System.currentTimeMillis() + CommonConstant.JWT_TTL_REFRESH);
             data.put("authorizationToken", authorizationToken);
-
+            redisHelper.set("u:"+userDto.getId()+":"+userDto.getAppUserId()+"",JwtUtil.parseJWT(authorizationToken).getIssuedAt().getTime(),CommonConstant.JWT_TTL, TimeUnit.MILLISECONDS);
             UserDto userJon=JSONObject.parseObject(subject, UserDto.class);
 
-            if (ValidatorUtil.notEmpty(user.getCellphone())){
-                userJon.setCellphone(user.getCellphone().replaceAll("(\\d{3})\\d{5}(\\d{3})", "$1****$2"));
+            if (ValidatorUtil.notEmpty(userDto.getCellphone())){
+                userJon.setCellphone(userDto.getCellphone().replaceAll("(\\d{3})\\d{5}(\\d{3})", "$1****$2"));
                 userJon.setIsCellphoneYN(1);
             }else{
                 userJon.setIsCellphoneYN(0);
             }
-            if (ValidatorUtil.notEmpty(user.getEmail())){
-                userJon.setEmail(user.getEmail().substring(0, 1) + "****" + user.getEmail().substring(user.getEmail().indexOf("@") - 1, user.getEmail().length()));
+            if (ValidatorUtil.notEmpty(userDto.getEmail())){
+                userJon.setEmail(userDto.getEmail().substring(0, 1) + "****" + userDto.getEmail().substring(userDto.getEmail().indexOf("@") - 1, userDto.getEmail().length()));
                 userJon.setIsEmailYN(1);
             }else{
                 userJon.setIsEmailYN(0);
             }
-            if(ValidatorUtil.notEmpty(user.getTradePwd())){
+            if(ValidatorUtil.notEmpty(userDto.getTradePwd())){
                 userJon.setIsTradePwdYN(1);
             } else{
                 userJon.setIsTradePwdYN(0);
             }
-            IdentityDto identityDto=identityService.findDataById(new IdentityDto(){{setUserId(user.getId());}});
+            IdentityDto identityDto=identityService.findDataById(new IdentityDto(){{setUserId(userDto.getId());}});
             if(identityDto!=null){
                 userJon.setRealName(identityDto.getRealName());
                 userJon.setCredentialNumber(identityDto.getCredentialNumber().substring(0, 3) + "***********" + identityDto.getCredentialNumber().substring(identityDto.getCredentialNumber().length()- 4, identityDto.getCredentialNumber().length()));
@@ -101,12 +124,16 @@ public class LoginController extends BaseController {
             data.put("user",userJon );
             try {
                 //读取session中的用户
-                UserDto actor = user;
                 UserLoginLogDto logDto = new UserLoginLogDto();
                 logDto.setType(0);//类型0登录1登出
                 logDto.setIpAddr(IpUtil.getIpAddr(request));//请求的IP
-                logDto.setUserId(actor.getId());//id
-                logDto.setUserName(actor.getName());//用户名称
+                logDto.setUserId(userDto.getId());//id
+                logDto.setUserName(userDto.getName());//用户名称
+                logDto.setUserName(userDto.getName());//员工名称
+                logDto.setAppStaffId(userDto.getAppUserId());//app用户id
+                logDto.setAppId(userDto.getAppId());//系统ID
+                logDto.setAppName(userDto.getAppName());
+
 //                logDto.setDeviceMac(IpUtil.getMACAddress(logDto.getIpAddr()));//MAC地址
                 loginLogService.saveOrUpdateData(logDto);
             } catch (Exception e) {
@@ -142,8 +169,13 @@ public class LoginController extends BaseController {
                 logDto.setIpAddr(IpUtil.getIpAddr(request));//请求的IP
                 logDto.setUserId(actor.getId());//id
                 logDto.setUserName(actor.getName());//用户名称
+                logDto.setAppName(actor.getAppName());
+                logDto.setAppUserId(actor.getAppUserId());
+                logDto.setAppId(actor.getAppId());
 //            logDto.setDeviceMac(IpUtil.getMACAddress(logDto.getIpAddr()));//MAC地址
                 loginLogService.saveOrUpdateData(logDto);
+
+                redisHelper.del("u:"+actor.getId()+":"+actor.getAppUserId()+"");
             } catch (Exception e) {
             }
             try {
@@ -176,6 +208,8 @@ public class LoginController extends BaseController {
 
             data.put("tokenExpMillis", System.currentTimeMillis() + CommonConstant.JWT_TTL_REFRESH);
             data.put("authorizationToken", refreshToken);
+            data.put("XCache", request.getSession().getId());
+            redisHelper.set("u:"+actor.getId()+":"+actor.getAppUserId()+"",JwtUtil.parseJWT(refreshToken).getIssuedAt().getTime(), CommonConstant.JWT_TTL, TimeUnit.MILLISECONDS);
             result.data = data;
         } catch (Exception e) {
             result = Response.error(e.getMessage());

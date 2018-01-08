60450ea74cd458f88ecf3fe9b009f5df0e8d1048
uname.chen <uname.chen@gmail.com>
2018-01-08 17:55:36 +0800

    portal

--- hsd-account/hsd-account-finance/hsd-account-finance-api/src/main/java/com/hsd/account/finance/api/IAccountBindThirdpartyService.java
+++ hsd-account/hsd-account-finance/hsd-account-finance-api/src/main/java/com/hsd/account/finance/api/IAccountBindThirdpartyService.java
@@ -7,7 +7,6 @@ import com.hsd.framework.config.FeignConfiguration;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 /**

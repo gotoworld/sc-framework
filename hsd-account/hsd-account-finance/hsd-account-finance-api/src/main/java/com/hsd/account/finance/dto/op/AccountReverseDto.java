3b92585e91bc42440b953eb007039e4e55630299
uname.chen <uname.chen@gmail.com>
2018-01-09 18:11:12 +0800

    账户功能开发-账户操作-冲正/抵扣

---
+++ hsd-account/hsd-account-finance/hsd-account-finance-api/src/main/java/com/hsd/account/finance/dto/op/AccountReverseDto.java
@@ -0,0 +1,17 @@
+package com.hsd.account.finance.dto.op;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.hsd.account.finance.dto.AccountLogFreezeDto;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel("账户-操作-冲正/抵扣 DTO")
+public class AccountReverseDto extends AccountLogFreezeDto {
+
+}

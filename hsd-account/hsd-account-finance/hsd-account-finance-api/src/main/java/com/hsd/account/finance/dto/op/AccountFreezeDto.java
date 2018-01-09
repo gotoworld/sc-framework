5f342e5c4d167cf213e29451ab991857af88bcb8
uname.chen <uname.chen@gmail.com>
2018-01-09 18:11:12 +0800

    账户功能开发-账户操作-冲正/抵扣

---
+++ hsd-account/hsd-account-finance/hsd-account-finance-api/src/main/java/com/hsd/account/finance/dto/op/AccountFreezeDto.java
@@ -0,0 +1,24 @@
+package com.hsd.account.finance.dto.op;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.hsd.account.finance.dto.AccountLogFreezeDto;
+import com.hsd.framework.dto.BaseDto;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@ApiModel("账户-操作-资金冻结记录 DTO")
+public class AccountFreezeDto extends AccountLogFreezeDto {
+
+}

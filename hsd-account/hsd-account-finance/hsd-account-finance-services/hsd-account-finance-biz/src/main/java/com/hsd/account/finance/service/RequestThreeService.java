e0dbb363ba3343a797a22be880f4cbe819b8ac35
yuankun <yuankun@heshidai.com>
2018-01-10 14:27:49 +0800

    卡三要素

---
+++ hsd-account/hsd-account-finance/hsd-account-finance-services/hsd-account-finance-biz/src/main/java/com/hsd/account/finance/service/RequestThreeService.java
@@ -0,0 +1,45 @@
+package com.hsd.account.finance.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.hsd.framework.net.HttpRestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+/**
+ * Created by hsd7 on 2018/1/10.
+ */
+@Service
+public class RequestThreeService {
+
+    @Autowired
+    private HttpRestClient httpRestClient;
+
+    //风控接口地址
+    @Value("${risk.url}")
+    private String riskUrl;
+
+    /**
+     * 卡三要素
+     * @param name 姓名
+     * @param certNo 身份证号码
+     * @param cardNo 银行卡号
+     * @return
+     */
+    public JSONObject cardThreeElement(String name, String certNo, String cardNo) throws Exception{
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+        map.add("userBankCard",cardNo);
+        map.add("userIdCard",certNo);
+        map.add("userName",name);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        String responseStr =  httpRestClient.doPost(riskUrl,map, String.class);
+        JSONObject responseJson = JSONObject.parseObject(responseStr);
+        return responseJson;
+    }
+}

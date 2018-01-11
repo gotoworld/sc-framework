dd08fe9aad5ef9b01265a37b5c518f059797ebad
yuankun <yuankun@heshidai.com>
2018-01-11 16:31:36 +0800

    冻结

--- hsd-account/hsd-account-finance/hsd-account-finance-services/hsd-account-finance-biz/src/main/java/com/hsd/account/finance/service/TransactionService.java
+++ hsd-account/hsd-account-finance/hsd-account-finance-services/hsd-account-finance-biz/src/main/java/com/hsd/account/finance/service/TransactionService.java
@@ -94,7 +94,9 @@ public class TransactionService extends BaseService implements ITransactionServi
                 result = Response.error("此账户状态不能进行扣款操作！!");
                 return result;
             }
-
+            userAccount.setTotalMoney(userAccount.getTotalMoney().divide(amount));
+            userAccount.setAvailableMoney(userAccount.getAvailableMoney().divide(amount));
+            accountDao.update(userAccount);
             Long id = idGenerator.nextId();
             AccountLog accountLog = new AccountLog();
             accountLog.setId(id);
@@ -103,143 +105,10 @@ public class TransactionService extends BaseService implements ITransactionServi
             accountLog.setAmount(amount);
             accountLog.setCurrency(0);
             accountLog.setOpAppUserId(userId);
-            accountLog.setState(2);
+            accountLog.setState(0);
             accountLogDao.insert(accountLog);
-
-
-
-            try{
-                //扣款
-                userAccount.setTotalMoney(userAccount.getTotalMoney().add(amount));
-                userAccount.setAvailableMoney(userAccount.getAvailableMoney().add(amount));
-                accountDao.update(userAccount);
-
-                accountLog.setState(0);
-
-            }catch (Exception e){
-                log.error("信息[详情]查询异常!", e);
-                accountLog.setState(3);
-                throw new ServiceException(SysErrorCode.defaultError,e.getMessage());
-            }
-            finally {
-                accountLogDao.update(accountLog);
-            }
         }catch (Exception e){
             log.error("账户[扣款]异常!", e);
-            throw new ServiceException(SysErrorCode.defaultError, e.getMessage());
-        }
-        finally {
-            lock.unlock();//释放锁
-        }
-        return result;
-    }
-
-
-    @Override
-    public Response amountFrozen(@RequestBody AccountDto dto) throws Exception {
-        Response result = new Response(0,"success");
-        Long accountId = dto.getId();
-        Long userId = dto.getAppUserId();
-        BigDecimal amount = dto.getFreezeMoney();
-        Lock lock = new RedisLock("lock:account-amountFrozen:"+accountId, 60 * 1000);
-        try{
-            Account account = new Account(){{setId(accountId);}};
-            Account  userAccount = (Account)accountDao.selectByPrimaryKey(account);
-            if(userAccount == null){
-                result = Response.error("账户不存在!");
-                return result;
-            }
-
-            if(userId != userAccount.getAppUserId()){
-                result = Response.error("用户账户不存在!");
-                return result;
-            }
-
-            if(userAccount.getState() == null){
-                result = Response.error("账户状态不正常!");
-                return result;
-            }
-
-            if(userAccount.getState().intValue() == 1){
-                result = Response.error("账户被冻结!");
-                return result;
-            }
-
-            if(userAccount.getState().intValue() == 2){
-                result = Response.error("账户被注销!");
-                return result;
-            }
-
-            if(userAccount.getAvailableMoney() == null || userAccount.getAvailableMoney().compareTo(amount) < 0){
-                result = Response.error("可用余额不足!");
-                return result;
-            }
-            userAccount.setAvailableMoney(userAccount.getAvailableMoney().divide(amount));
-            userAccount.setFreezeMoney(userAccount.getFreezeMoney().add(amount));
-            accountDao.update(userAccount);
-
-
-            AccountLogFreeze accountLogFreeze = new AccountLogFreeze();
-            accountLogFreeze.setId(idGenerator.nextId());
-            accountLogFreeze.setAppUserId(userId);
-            accountLogFreeze.setAccountId(accountId);
-            accountLogFreeze.setAmount(amount);
-            accountLogFreezeDao.insert(accountLogFreeze);
-        }catch (Exception e){
-            log.error("账户资金[冻结]异常!", e);
-            throw new ServiceException(SysErrorCode.defaultError, e.getMessage());
-        }
-        finally {
-            lock.unlock();//释放锁
-        }
-        return result;
-    }
-
-
-    @Override
-    public Response amountUnfreeze(@RequestBody AccountDto dto) throws Exception {
-        Response result = new Response(0,"success");
-        Long accountId = dto.getId();
-        Long userId = dto.getAppUserId();
-        BigDecimal amount = dto.getFreezeMoney();
-        Lock lock = new RedisLock("lock:account-amountUnfreeze:"+accountId, 60 * 1000);
-        try{
-            Account account = new Account(){{setId(accountId);}};
-            Account  userAccount = (Account)accountDao.selectByPrimaryKey(account);
-            if(userAccount == null){
-                result = Response.error("账户不存在!");
-                return result;
-            }
-
-            if(userId != userAccount.getAppUserId()){
-                result = Response.error("用户账户不存在!");
-                return result;
-            }
-
-            if(userAccount.getState() == null){
-                result = Response.error("账户状态不正常!");
-                return result;
-            }
-
-            if(userAccount.getState().intValue() == 1){
-                result = Response.error("账户被冻结!");
-                return result;
-            }
-
-            if(userAccount.getState().intValue() == 2){
-                result = Response.error("账户被注销!");
-                return result;
-            }
-
-            if(userAccount.getFreezeMoney() == null || userAccount.getFreezeMoney().compareTo(amount) < 0){
-                result = Response.error("冻结金额不正确!");
-                return result;
-            }
-            userAccount.setAvailableMoney(userAccount.getAvailableMoney().add(amount));
-            userAccount.setFreezeMoney(userAccount.getFreezeMoney().divide(amount));
-            accountDao.update(userAccount);
-        }catch (Exception e){
-            log.error("账户资金[解冻]异常!", e);
             throw new ServiceException(SysErrorCode.defaultError, e.getMessage());
         }
         finally {

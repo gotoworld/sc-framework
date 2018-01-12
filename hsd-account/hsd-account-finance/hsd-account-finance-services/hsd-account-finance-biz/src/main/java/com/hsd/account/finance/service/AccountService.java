27080859f8de2fd953757d82390888d3d8c18a23
uname.chen <uname.chen@gmail.com>
2018-01-12 16:48:28 +0800

    +分布式锁

--- hsd-account/hsd-account-finance/hsd-account-finance-services/hsd-account-finance-biz/src/main/java/com/hsd/account/finance/service/AccountService.java
+++ hsd-account/hsd-account-finance/hsd-account-finance-services/hsd-account-finance-biz/src/main/java/com/hsd/account/finance/service/AccountService.java
@@ -200,59 +200,63 @@ public class AccountService extends BaseService implements IAccountService {
                 return Response.error("操作类型未知");
             }
             if(lock.tryLock(50*1000)) {//加锁块
-                //TODO 获取用户资金账户绑定银行卡
-                //TODO 调用接口执行转账
+                try {
+                    //TODO 获取用户资金账户绑定银行卡
+                    //TODO 调用接口执行转账
 
-                //系统账户快照更新-获取指定账户信息
-                switch (dto.getBizAccountType()) {//业务账户
-                    case 0: { //0资金账户
-                        //获取原始账户信息
-                        Account entity = (Account) accountDao.selectByPrimaryKey(new Account() {{
-                            setId(dto.getId());
-                        }});
-                        entity.setFreezeMoney(dto.getAmount());
-                        if (dto.getActionType() == 0) {//冻结 原始冻结金额+本次冻结金额
-                            if (accountDao.freeze(entity) == 0) return Response.error("可用金额不足");
-                        } else {//解冻 原始冻结金额-本次冻结金额
-                            if (accountDao.unfreeze(entity) == 0) return Response.error("可解冻金额不足");
+                    //系统账户快照更新-获取指定账户信息
+                    switch (dto.getBizAccountType()) {//业务账户
+                        case 0: { //0资金账户
+                            //获取原始账户信息
+                            Account entity = (Account) accountDao.selectByPrimaryKey(new Account() {{
+                                setId(dto.getId());
+                            }});
+                            entity.setFreezeMoney(dto.getAmount());
+                            if (dto.getActionType() == 0) {//冻结 原始冻结金额+本次冻结金额
+                                if (accountDao.freeze(entity) == 0) return Response.error("可用金额不足");
+                            } else {//解冻 原始冻结金额-本次冻结金额
+                                if (accountDao.unfreeze(entity) == 0) return Response.error("可解冻金额不足");
+                            }
+                            break;
                         }
-                        break;
-                    }
-                    case 1: {//1黄金账户
-                        //AccountSubGold entity= (AccountSubGold) accountSubGoldDao.selectByPrimaryKey(new AccountSubGold(){{setId(dto.getId());}});
-                        //entity.setFreezeMoney(freezeAccount(dto.getActionType(),entity.getFreezeMoney(),dto.getAmount()));
-                        //accountSubGoldDao.freeze(entity);
-                        return Response.error("黄金账户,暂无[冻结/解冻]功能");
-                        // break;
-                    }
-                    case 2: { //2网贷账户
-                        AccountSubLoan entity = (AccountSubLoan) accountSubLoanDao.selectByPrimaryKey(new AccountSubLoan() {{
-                            setId(dto.getId());
-                        }});
-                        entity.setFreezeMoney(dto.getAmount());
-                        if (dto.getActionType() == 0) {//冻结 原始冻结金额+本次冻结金额
-                            if (accountSubLoanDao.freeze(entity) == 0) return Response.error("可用金额不足");
-                        } else {//解冻 原始冻结金额-本次冻结金额
-                            if (accountSubLoanDao.unfreeze(entity) == 0) return Response.error("可解冻金额不足");
+                        case 1: {//1黄金账户
+                            //AccountSubGold entity= (AccountSubGold) accountSubGoldDao.selectByPrimaryKey(new AccountSubGold(){{setId(dto.getId());}});
+                            //entity.setFreezeMoney(freezeAccount(dto.getActionType(),entity.getFreezeMoney(),dto.getAmount()));
+                            //accountSubGoldDao.freeze(entity);
+                            return Response.error("黄金账户,暂无[冻结/解冻]功能");
+                            // break;
                         }
-                        break;
+                        case 2: { //2网贷账户
+                            AccountSubLoan entity = (AccountSubLoan) accountSubLoanDao.selectByPrimaryKey(new AccountSubLoan() {{
+                                setId(dto.getId());
+                            }});
+                            entity.setFreezeMoney(dto.getAmount());
+                            if (dto.getActionType() == 0) {//冻结 原始冻结金额+本次冻结金额
+                                if (accountSubLoanDao.freeze(entity) == 0) return Response.error("可用金额不足");
+                            } else {//解冻 原始冻结金额-本次冻结金额
+                                if (accountSubLoanDao.unfreeze(entity) == 0) return Response.error("可解冻金额不足");
+                            }
+                            break;
+                        }
                     }
+                    //.记录冻结日志
+                    AccountLogFreeze logFreezeEntity = copyTo(dto, AccountLogFreeze.class);
+                    logFreezeEntity.setId(idGenerator.nextId());
+                    logFreezeDao.insert(logFreezeEntity);
+
+                    //.记录操作日志
+                    AccountLogOperational logOperational = copyTo(dto, AccountLogOperational.class);
+                    logOperational.setId(idGenerator.nextId());
+                    logOperational.setData(JSON.toJSONString(dto));
+                    logOperational.setType(dto.getActionType() == 0 ? 1 : 2);//1冻结,2解冻
+                    logOperational.setMemo("" + (dto.getActionType() == 0 ? "[冻结]" : "[解冻]") + dto.getAmount());
+                    logOperational.setUserName(dto.getStaffName());
+                    logOperational.setCreateId(dto.getCreateId());
+                    logOperational.setCreateIp(dto.getIp());
+                    logOperationalDao.insert(logOperational);
+                }finally {
+                    lock.unlock();//释放锁
                 }
-                //.记录冻结日志
-                AccountLogFreeze logFreezeEntity = copyTo(dto, AccountLogFreeze.class);
-                logFreezeEntity.setId(idGenerator.nextId());
-                logFreezeDao.insert(logFreezeEntity);
-
-                //.记录操作日志
-                AccountLogOperational logOperational = copyTo(dto, AccountLogOperational.class);
-                logOperational.setId(idGenerator.nextId());
-                logOperational.setData(JSON.toJSONString(dto));
-                logOperational.setType(dto.getActionType() == 0 ? 1 : 2);//1冻结,2解冻
-                logOperational.setMemo("" + (dto.getActionType() == 0 ? "[冻结]" : "[解冻]") + dto.getAmount());
-                logOperational.setUserName(dto.getStaffName());
-                logOperational.setCreateId(dto.getCreateId());
-                logOperational.setCreateIp(dto.getIp());
-                logOperationalDao.insert(logOperational);
             }
         } catch (Exception e) {
             log.error("账户[冻结/解冻]异常!", e);
@@ -276,45 +280,49 @@ public class AccountService extends BaseService implements IAccountService {
                 return Response.error("业务账户未知");
             }
             if(lock.tryLock(50*1000)) {//加锁块
-                //获取指定账户信息
-                switch (dto.getBizAccountType()) {//业务账户
-                    case 0: { //0资金账户
-                        return Response.error("资金账户,暂无[充值]功能");
+                try {
+                    //获取指定账户信息
+                    switch (dto.getBizAccountType()) {//业务账户
+                        case 0: { //0资金账户
+                            return Response.error("资金账户,暂无[充值]功能");
+                        }
+                        case 1: {//1黄金账户
+                            AccountSubGold entity = (AccountSubGold) accountSubGoldDao.selectByPrimaryKey(new AccountSubGold() {{
+                                setId(dto.getId());
+                            }});
+                            if (entity == null) {
+                                return Response.error("未找到黄金账户！");
+                            }
+                            if (entity.getState() != 0) {
+                                return Response.error("黄金账户不正常！");
+                            }
+                            entity.setTotalGold(dto.getAccountMoney());
+                            if (accountSubGoldDao.recharge(entity) == 0) return Response.error("未找到黄金账户或账户状态不正常!");
+                            break;
+                        }
+                        case 2: { //2网贷账户
+                            AccountSubLoan entity = (AccountSubLoan) accountSubLoanDao.selectByPrimaryKey(new AccountSubLoan() {{
+                                setId(dto.getId());
+                            }});
+                            if (entity == null) {
+                                return Response.error("未找到网贷账户！");
+                            }
+                            if (entity.getState() != 0) {
+                                return Response.error("网贷账户不正常！");
+                            }
+                            entity.setAvailableMoney(dto.getAccountMoney());
+                            entity.setTotalMoney(dto.getAccountMoney());
+                            if (accountSubLoanDao.recharge(entity) == 0) return Response.error("未找到网贷账户或账户状态不正常!");
+                            break;
+                        }
                     }
-                    case 1: {//1黄金账户
-                        AccountSubGold entity = (AccountSubGold) accountSubGoldDao.selectByPrimaryKey(new AccountSubGold() {{
-                            setId(dto.getId());
-                        }});
-                        if (entity == null) {
-                            return Response.error("未找到黄金账户！");
-                        }
-                        if (entity.getState() != 0) {
-                            return Response.error("黄金账户不正常！");
-                        }
-                        entity.setTotalGold(dto.getAccountMoney());
-                        if (accountSubGoldDao.recharge(entity) == 0) return Response.error("未找到黄金账户或账户状态不正常!");
-                        break;
-                    }
-                    case 2: { //2网贷账户
-                        AccountSubLoan entity = (AccountSubLoan) accountSubLoanDao.selectByPrimaryKey(new AccountSubLoan() {{
-                            setId(dto.getId());
-                        }});
-                        if (entity == null) {
-                            return Response.error("未找到网贷账户！");
-                        }
-                        if (entity.getState() != 0) {
-                            return Response.error("网贷账户不正常！");
-                        }
-                        entity.setAvailableMoney(dto.getAccountMoney());
-                        entity.setTotalMoney(dto.getAccountMoney());
-                        if (accountSubLoanDao.recharge(entity) == 0) return Response.error("未找到网贷账户或账户状态不正常!");
-                        break;
-                    }
+                    //.记录充值日志
+                    AccountLogRecharge logRechargeEntity = copyTo(dto, AccountLogRecharge.class);
+                    logRechargeEntity.setId(idGenerator.nextId());
+                    logRechargeDao.insert(logRechargeEntity);
+                }finally {
+                    lock.unlock();//释放锁
                 }
-                //.记录充值日志
-                AccountLogRecharge logRechargeEntity = copyTo(dto, AccountLogRecharge.class);
-                logRechargeEntity.setId(idGenerator.nextId());
-                logRechargeDao.insert(logRechargeEntity);
             }
         } catch (Exception e) {
             log.error("账户[充值]异常!", e);
@@ -337,45 +345,49 @@ public class AccountService extends BaseService implements IAccountService {
                 return Response.error("业务账户未知");
             }
             if(lock.tryLock(50*1000)) {//加锁块
-                //获取指定账户信息
-                switch (dto.getBizAccountType()) {//业务账户
-                    case 0: { //0资金账户
-                        return Response.error("资金账户,暂无[提现]功能");
+                try {
+                    //获取指定账户信息
+                    switch (dto.getBizAccountType()) {//业务账户
+                        case 0: { //0资金账户
+                            return Response.error("资金账户,暂无[提现]功能");
+                        }
+                        case 1: {//1黄金账户
+                            AccountSubGold entity = (AccountSubGold) accountSubGoldDao.selectByPrimaryKey(new AccountSubGold() {{
+                                setId(dto.getId());
+                            }});
+                            if (entity == null) {
+                                return Response.error("未找到黄金账户！");
+                            }
+                            if (entity.getState() != 0) {
+                                return Response.error("黄金账户不正常！");
+                            }
+                            entity.setTotalGold(dto.getMoney());
+                            if (accountSubGoldDao.withdrawal(entity) == 0) return Response.error("未找到黄金账户或账户状态不正常!");
+                            break;
+                        }
+                        case 2: { //2网贷账户
+                            AccountSubLoan entity = (AccountSubLoan) accountSubLoanDao.selectByPrimaryKey(new AccountSubLoan() {{
+                                setId(dto.getId());
+                            }});
+                            if (entity == null) {
+                                return Response.error("未找到网贷账户！");
+                            }
+                            if (entity.getState() != 0) {
+                                return Response.error("网贷账户不正常！");
+                            }
+                            entity.setAvailableMoney(dto.getMoney());
+                            entity.setTotalMoney(dto.getMoney());
+                            if (accountSubLoanDao.withdrawal(entity) == 0) return Response.error("未找到网贷账户或账户状态不正常!");
+                            break;
+                        }
                     }
-                    case 1: {//1黄金账户
-                        AccountSubGold entity = (AccountSubGold) accountSubGoldDao.selectByPrimaryKey(new AccountSubGold() {{
-                            setId(dto.getId());
-                        }});
-                        if (entity == null) {
-                            return Response.error("未找到黄金账户！");
-                        }
-                        if (entity.getState() != 0) {
-                            return Response.error("黄金账户不正常！");
-                        }
-                        entity.setTotalGold(dto.getMoney());
-                        if (accountSubGoldDao.withdrawal(entity) == 0) return Response.error("未找到黄金账户或账户状态不正常!");
-                        break;
-                    }
-                    case 2: { //2网贷账户
-                        AccountSubLoan entity = (AccountSubLoan) accountSubLoanDao.selectByPrimaryKey(new AccountSubLoan() {{
-                            setId(dto.getId());
-                        }});
-                        if (entity == null) {
-                            return Response.error("未找到网贷账户！");
-                        }
-                        if (entity.getState() != 0) {
-                            return Response.error("网贷账户不正常！");
-                        }
-                        entity.setAvailableMoney(dto.getMoney());
-                        entity.setTotalMoney(dto.getMoney());
-                        if (accountSubLoanDao.withdrawal(entity) == 0) return Response.error("未找到网贷账户或账户状态不正常!");
-                        break;
-                    }
+                    //.记录提现日志
+                    AccountLogWithdrawal logWithdrawalEntity = copyTo(dto, AccountLogWithdrawal.class);
+                    logWithdrawalEntity.setId(idGenerator.nextId());
+                    logWithdrawalDao.insert(logWithdrawalEntity);
+                }finally {
+                    lock.unlock();//释放锁
                 }
-                //.记录提现日志
-                AccountLogWithdrawal logWithdrawalEntity = copyTo(dto, AccountLogWithdrawal.class);
-                logWithdrawalEntity.setId(idGenerator.nextId());
-                logWithdrawalDao.insert(logWithdrawalEntity);
             }
         } catch (Exception e) {
             log.error("账户[提现]异常!", e);

f1d093fe33f0c19324ce1b5571f054d4ae947c8c
uname.chen <uname.chen@gmail.com>
2018-01-15 14:32:59 +0800

    开户

--- hsd-account/hsd-account-finance/hsd-account-finance-web-portal/src/main/java/com/hsd/account/finance/web/controller/AccountSubLoanController.java
+++ hsd-account/hsd-account-finance/hsd-account-finance-web-portal/src/main/java/com/hsd/account/finance/web/controller/AccountSubLoanController.java
@@ -2,19 +2,12 @@ package com.hsd.account.finance.web.controller;
 
 import com.hsd.account.finance.api.IAccountSubLoanService;
 import com.hsd.account.finance.dto.AccountSubLoanDto;
-import com.hsd.framework.PageUtil;
 import com.hsd.framework.Response;
-import com.hsd.framework.annotation.auth.Logical;
-import com.hsd.framework.annotation.auth.RequiresPermissions;
-import com.hsd.framework.util.CommonConstant;
 import com.hsd.framework.web.controller.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -49,27 +42,17 @@ public class AccountSubLoanController extends BaseController {
     /**
      * <p> 开户信息保存
      */
-    @RequestMapping(method={RequestMethod.GET,RequestMethod.POST},value = acPrefix + "save")
+    @RequestMapping(method={RequestMethod.GET,RequestMethod.POST},value = acPrefix + "open/{userId}")
     @ApiOperation(value = "信息保存")
-    public Response save(@Validated @ModelAttribute AccountSubLoanDto dto, BindingResult bindingResult) {
-        log.info("AccountSubLoanController save.........");
+    public Response open(@PathVariable("userId") Long userId, @RequestParam Long accountType) {
+        log.info("AccountSubLoanController open.........");
         Response result = new Response(0,"success");
         try {
-            if (dto == null)throw new RuntimeException("参数异常");
-            if ("1".equals(request.getSession().getAttribute(acPrefix + "save." + dto.getToken()))) {
-                throw new RuntimeException("请不要重复提交!");
-            }
-            if (bindingResult.hasErrors()) {
-                String errorMsg = "";
-                List<ObjectError> errorList = bindingResult.getAllErrors();
-                for (ObjectError error : errorList) {
-                    errorMsg += (error.getDefaultMessage()) + ";";
-                }
-                result = Response.error(errorMsg);
-            } else {
-                result = accountSubLoanService.saveOrUpdateData(dto);
-                request.getSession().setAttribute(acPrefix + "save." + dto.getToken(), "1");
-            }
+            AccountSubLoanDto dto = new AccountSubLoanDto(){{
+                setAppUserId(userId);
+                setAccountType(accountType);
+            }};
+            result.data = accountSubLoanService.open(dto);
         } catch (Exception e) {
             result = Response.error(e.getMessage());
         }

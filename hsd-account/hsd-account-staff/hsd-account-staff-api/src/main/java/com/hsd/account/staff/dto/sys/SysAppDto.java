b80aa704d9d115f60463393bb19a6c54bd7953e3
uname.chen <uname.chen@gmail.com>
2018-01-22 09:43:27 +0800

    应用管理/修复错误属性注释

--- hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/dto/sys/SysAppDto.java
+++ hsd-account/hsd-account-staff/hsd-account-staff-api/src/main/java/com/hsd/account/staff/dto/sys/SysAppDto.java
@@ -18,32 +18,36 @@ import java.util.Date;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @ApiModel("应用系统表 DTO")
 public class SysAppDto extends BaseDto {
-     @ApiModelProperty("appId")
-	 @Size(max=22,message="id最大22字符")
-     private String id;
-     @ApiModelProperty("系统名称")
-	 @NotNull(message="name不能为空")@Size(max=50,message="name最大50字符")
-     private String name;
-     @ApiModelProperty("主页登链")
-	 @NotNull(message="login_url不能为空")@Size(max=255,message="login_url最大255字符")
-     private String loginUrl;
-     @ApiModelProperty("登录链接")
-	 @Size(max=255,message="main_url最大255字符")
-     private String mainUrl;
-     @ApiModelProperty("备注")
-	 @Size(max=255,message="memo最大255字符")
-     private String memo;
+    @ApiModelProperty("appId")
+    @Size(max = 22, message = "id最大22字符")
+    private String id;
+    @ApiModelProperty("系统名称")
+    @NotNull(message = "name不能为空")
+    @Size(max = 50, message = "name最大50字符")
+    private String name;
+    @ApiModelProperty("登录链接")
+    @NotNull(message = "login_url不能为空")
+    @Size(max = 255, message = "login_url最大255字符")
+    private String loginUrl;
+    @ApiModelProperty("主页登链")
+    @Size(max = 255, message = "main_url最大255字符")
+    private String mainUrl;
+    @ApiModelProperty("备注")
+    @Size(max = 255, message = "memo最大255字符")
+    private String memo;
     @ApiModelProperty("是否删除")
     private Integer delFlag;
-    @Size(max=255,message="memo最大255字符")
-     private String keyword;
+    @Size(max = 255, message = "memo最大255字符")
+    private String keyword;
     @ApiModelProperty("建立者id")
-     private Long createId;
+    private Long createId;
     @ApiModelProperty("创建时间")
-     private Date dateCreated;
+    private Date dateCreated;
     @ApiModelProperty("更新时间")
-     private Date dateUpdated;
-    /**已存在的禁止删除(0否1是)*/
+    private Date dateUpdated;
+    /**
+     * 已存在的禁止删除(0否1是)
+     */
     private Integer noDelFlag;
 
 }
